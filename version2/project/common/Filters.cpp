b665733f01b6d5308b58408493d458062aa4ea9f
Hugh Sanderson <gamehaxe@gmail.com>
2010-03-10 12:07:48 +0000

    Fix BlurFilter
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@383 1509560c-5e2a-0410-865c-31c25e1cfdef

--- version2/project/common/Filters.cpp
+++ version2/project/common/Filters.cpp
@@ -31,7 +31,7 @@ void BlurFilter::GetFilteredObjectRect(Rect &ioRect) const
 */
 
 void BlurRow(const ARGB *inSrc, int inDS, int inSrcW, int inOffX,
-				 ARGB *inDest, int inDD, int inDestW, int inFilterSize)
+             ARGB *inDest, int inDD, int inDestW, int inFilterSize)
 {
    int sc0 = 0;
    int sc1 = 0;
@@ -40,19 +40,19 @@ void BlurRow(const ARGB *inSrc, int inDS, int inSrcW, int inOffX,
 
    // loop over destination pixels with kernel    -xxx+
    // At each pixel, we - the trailing pixel and + the leading pixel
-   const ARGB *prev = inSrc - inOffX*inDS - inFilterSize*inDS;
-   const ARGB *src = inSrc - inOffX*inDS + inFilterSize*inDS;
+   const ARGB *prev = inSrc + inOffX*inDS - inFilterSize*inDS;
+   const ARGB *src = inSrc + inOffX*inDS + inFilterSize*inDS;
    const ARGB *src_end = inSrc + inSrcW*inDS;
    ARGB *dest = inDest;
-	int fs = inFilterSize*2+1;
-	for(const ARGB *s=inSrc;s<src;s+=inDS)
-	{
-		int a = s->a;
+   int fs = inFilterSize*2+1;
+   for(const ARGB *s=inSrc;s<src;s+=inDS)
+   {
+      int a = s->a;
       sa+=a;
       sc0+= s->c0 * a;
       sc1+= s->c1 * a;
       sc2+= s->c2 * a;
-	}
+   }
    for(int x=0;x<inDestW; x++)
    {
       if (src>=inSrc && src<src_end)
@@ -63,7 +63,10 @@ void BlurRow(const ARGB *inSrc, int inDS, int inSrcW, int inOffX,
          sc1+= src->c1 * a;
          sc2+= src->c2 * a;
       }
-      if (prev>=inSrc && prev<src_end)
+      if (prev>=src_end)
+         return;
+
+      if (prev>=inSrc)
       {
          int a = prev->a;
          sa-=a;
@@ -94,56 +97,48 @@ void BlurFilter::Apply(const Surface *inSrc,Surface *outDest, ImagePoint inDiff)
 {
    int w = outDest->Width();
    int h = outDest->Height();
-	int sw = inSrc->Width();
-	int sh = inSrc->Height();
+   int sw = inSrc->Width();
+   int sh = inSrc->Height();
 
-	Surface *tmp = new SimpleSurface(w,h,outDest->Format());
-	tmp->IncRef();
+   outDest->Zero();
 
-	{
-	AutoSurfaceRender tmp_render(tmp);
-	const RenderTarget &target = tmp_render.Target();
+   Surface *tmp = new SimpleSurface(sw+2*mBlurX,sh,outDest->Format());
+   tmp->IncRef();
+
+   {
+   AutoSurfaceRender tmp_render(tmp);
+   const RenderTarget &target = tmp_render.Target();
    // Blur rows ...
-   for(int y=0;y<h;y++)
+   for(int y=0;y<sh;y++)
    {
       ARGB *dest = (ARGB *)target.Row(y);
-		int src_y = y + inDiff.y;
-		if (src_y<0 || src_y>=sh)
-			memset(dest, 0, w*sizeof(ARGB));
-		else
-		{
-         const ARGB *src = ((ARGB *)inSrc->Row(src_y));
-         BlurRow(src,1,sw,inDiff.x,dest,1,w,mBlurX);
-		}
+      const ARGB *src = ((ARGB *)inSrc->Row(y));
+      BlurRow(src,1,sw,-mBlurX,dest,1,tmp->Width(),mBlurX);
    }
-	}
+   inDiff.x += mBlurX;
+   sw = tmp->Width();
+   }
 
-
-	{
-	AutoSurfaceRender dest_render(outDest);
-	const RenderTarget &target = dest_render.Target();
-	int s_stride = tmp->GetStride()/sizeof(ARGB);
-	int d_stride = target.mSoftStride/sizeof(ARGB);
+   {
+   AutoSurfaceRender dest_render(outDest);
+   const RenderTarget &target = dest_render.Target();
+   int s_stride = tmp->GetStride()/sizeof(ARGB);
+   int d_stride = target.mSoftStride/sizeof(ARGB);
    // Blur cols ...
    for(int x=0;x<w;x++)
    {
-      ARGB *dest = (ARGB *)target.Row(0) + x;
-		int src_x = x + inDiff.x;
-		if (src_x<0 || src_x>=sw)
-			for(int y=0;y<h;y++)
-			{
-				dest->ival = 0; //0xff000000 | y*0x10101;
-				dest+=d_stride;
-			}
-		else
-		{
-      	const ARGB *src = ((ARGB *)tmp->Row(0)) + src_x;
-         BlurRow(src,s_stride,sh,inDiff.y,dest,d_stride,h,mBlurY);
-		}
-   }
-	}
+      int src_x = x - inDiff.x;
+      if (src_x>=0 && src_x<sw)
+      {
+         ARGB *dest = (ARGB *)target.Row(0) + x;
+         const ARGB *src = ((ARGB *)tmp->Row(0)) + src_x;
 
-	tmp->DecRef();
+         BlurRow(src,s_stride,sh,inDiff.y,dest,d_stride,h,mBlurY);
+      }
+   }
+   }
+
+   tmp->DecRef();
 }
 
 
@@ -198,8 +193,6 @@ Surface *FilterBitmap( const FilterList &inFilters, Surface *inBitmap,
 
       Surface *filtered = new SimpleSurface(dest_rect.w,dest_rect.h,bmp->Format());
 
-      filtered->Zero();
-  
       inFilters[i]->Apply(bmp,filtered, ImagePoint(dest_rect.x-src_rect.x,
                                                    dest_rect.y-src_rect.y) );
 
