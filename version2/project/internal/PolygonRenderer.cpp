627a32b5a3fc21c89f245cbb41bc2db4bba4caa2
Hugh Sanderson <gamehaxe@gmail.com>
2010-04-25 10:51:13 +0000

    Start on software triangles
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@437 1509560c-5e2a-0410-865c-31c25e1cfdef

--- version2/project/internal/PolygonRenderer.cpp
+++ version2/project/internal/PolygonRenderer.cpp
@@ -307,6 +307,7 @@ struct SpanRect
 };
 
 
+// --- PolygonRender Base -------------------------------------------------------------
 
 
 class PolygonRender : public CachedExtentRenderer
@@ -359,7 +360,7 @@ public:
    }
 
 
-   void SetTransform(const Transform &inTransform)
+   virtual void SetTransform(const Transform &inTransform)
    {
       int points = mDataCount/2;
       if (points!=mTransformed.size() || inTransform!=mTransform)
@@ -400,7 +401,7 @@ public:
       CachedExtentRenderer::GetExtent(inState.mTransform,extent);
 
       if (!extent.Valid())
-         return true;
+			return true;
 
       // Get bounding pixel rect
       Rect rect = inState.mTransform.GetTargetRect(extent);
@@ -1089,6 +1090,139 @@ public:
    }
 };
 
+class TriangleRender :public PolygonRender
+{
+public:
+	TriangleRender(const GraphicsJob &inJob, const GraphicsPath &inPath ):
+       PolygonRender(inJob, inPath, inJob.mFill)
+   {
+		mTriangles = inJob.mTriangles;
+   }
+
+	void SetTransform(const Transform &inTransform)
+   {
+      int points = mTriangles->mVertices.size();
+      if (points!=mTransformed.size() || inTransform!=mTransform)
+      {
+         mTransform = inTransform;
+         mTransMat = *inTransform.mMatrix;
+         mTransform.mMatrix = &mTransMat;
+         mTransform.mMatrix3D = &mTransMat;
+         mTransScale9 = *inTransform.mScale9;
+         mTransform.mScale9 = &mTransScale9;
+         mTransformed.resize(points);
+         UserPoint *src= (UserPoint *)&mTriangles->mVertices[ 0 ];
+         for(int i=0;i<points;i++)
+            mTransformed[i] = mTransform.Apply(src[i].x,src[i].y);
+      }
+   }
+
+	bool Render(const RenderTarget &inTarget, const RenderState &inState)
+   {
+		if (mTriangles->mUVT.empty())
+			return PolygonRender::Render(inTarget,inState);
+
+      Extent2DF extent;
+      CachedExtentRenderer::GetExtent(inState.mTransform,extent);
+
+      if (!extent.Valid())
+			return true;
+
+      // Get bounding pixel rect
+      Rect rect = inState.mTransform.GetTargetRect(extent);
+
+      // Intersect with clip rect ...
+      Rect visible_pixels = rect.Intersect(inState.mClipRect);
+
+		/*
+
+      // For each alpha mask ...
+      // Check to see if AlphaMask is invalid...
+      int tx=0;
+      int ty=0;
+      if (mAlphaMask && !mAlphaMask->Compatible(inState.mTransform, rect,visible_pixels,tx,ty))
+      {
+         delete mAlphaMask;
+         mAlphaMask = 0;
+      }
+
+      if (!mAlphaMask)
+      {
+         SetTransform(inState.mTransform);
+
+         // TODO: make visible_pixels a bit bigger ?
+         mSpanRect = new SpanRect(visible_pixels,inState.mTransform.mAAFactor);
+
+         Iterate(itCreateRenderer,*inState.mTransform.mMatrix);
+
+         mAlphaMask = mSpanRect->CreateMask(mTransform);
+         delete mSpanRect;
+      }
+
+      if (inTarget.mPixelFormat==pfAlpha)
+      {
+         mAlphaMask->RenderBitmap(tx,ty,inTarget,inState);
+      }
+      else
+         mFiller->Fill(*mAlphaMask,tx,ty,inTarget,inState);
+
+      return true;
+
+
+
+
+		int tris = mTriangles->mTriangleCount;
+		for(int t=0;t<tris;t++)
+		{
+			(*this.*func)(point[0],point[1]);
+			(*this.*func)(point[1],point[2]);
+			(*this.*func)(point[2],point[0]);
+			point += 3;
+		}
+
+		*/
+	}
+
+
+
+	void Iterate(IterateMode inMode,const Matrix &m)
+   {
+      const UserPoint *point = 0;
+
+      if (inMode==itHitTest)
+         point = (const UserPoint *)&mTriangles->mVertices[0];
+      else
+         point = &mTransformed[0];
+
+
+      int points = mTriangles->mVertices.size();
+      if (inMode==itGetExtent)
+      {
+			for(int p=0;p<points;p++)
+			   mBuildExtent->Add(point[p]);
+		}
+		else
+		{
+			typedef void (PolygonRender::*ItFunc)(const UserPoint &inP0, const UserPoint &inP1);
+         ItFunc func = inMode==itCreateRenderer ? &PolygonRender::BuildSolid :
+                  &PolygonRender::BuildHitTest;
+
+			int tris = mTriangles->mTriangleCount;
+			for(int t=0;t<tris;t++)
+			{
+				(*this.*func)(point[0],point[1]);
+				(*this.*func)(point[1],point[2]);
+				(*this.*func)(point[2],point[0]);
+				point += 3;
+			}
+		}
+   }
+
+	GraphicsTrianglePath *mTriangles;
+};
+
+// --- TileRenderer --------------------------------------------------------------------
+
 
 class TileRenderer : public Renderer
 {
@@ -1368,8 +1502,8 @@ public:
 
 Renderer *Renderer::CreateSoftware(const GraphicsJob &inJob, const GraphicsPath &inPath)
 {
-   if (inJob.mIsTileJob)
-      return new TileRenderer(inJob,inPath);
+   if (inJob.mTriangles)
+      return new TriangleRender(inJob,inPath);
 	else if (inJob.mIsTileJob)
       return new TileRenderer(inJob,inPath);
    else if (inJob.mIsPointJob)
