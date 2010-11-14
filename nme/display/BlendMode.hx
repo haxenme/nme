aa0b5d4c225d9a2653b3348a949e0d1c8ab774e6
Hugh Sanderson <gamehaxe@gmail.com>
2010-11-14 14:45:10 +0000

    Added blendModes
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@534 1509560c-5e2a-0410-865c-31c25e1cfdef

---
+++ nme/display/BlendMode.hx
@@ -0,0 +1,20 @@
+package nme.display;
+
+enum BlendMode
+{
+   NORMAL;
+   LAYER;
+   MULTIPLY;
+   SCREEN;
+   LIGHTEN;
+   DARKEN;
+   DIFFERENCE;
+   ADD;
+   SUBTRACT;
+   INVERT;
+   ALPHA;
+   ERASE;
+   OVERLAY;
+   HARDLIGHT;
+}
+
