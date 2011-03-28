b06d48735a804d2bb67cb7b181902018550b1137
Hugh Sanderson <gamehaxe@gmail.com>
2011-03-28 13:18:19 +0000

    Add black and white versions
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@609 1509560c-5e2a-0410-865c-31c25e1cfdef

--- samples/02-Text/Sample.hx
+++ samples/02-Text/Sample.hx
@@ -5,30 +5,42 @@ class Sample
 
 public static function main()
 {
-   var text = new flash.text.TextField();
-   text.x = 10;
-   text.y = 10;
-   text.text = "Hello !";
-   Lib.current.stage.addChild(text);
-   text.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+   var gfx = Lib.current.graphics;
+   gfx.beginFill(0x000000);
+   gfx.drawRect(120,0,120,120);
 
-   var text = new flash.text.TextField();
-   text.x = 10;
-   text.y = 30;
-   text.htmlText = "<font size='16'>Hello !</font>";
-   Lib.current.stage.addChild(text);
+   for(side in 0...2)
+   {
+      var col = side * 0xffffff;
+      var text = new flash.text.TextField();
+      text.x = 10 + side*120;
+      text.y = 10;
+      text.textColor = col;
+      text.text = "Hello !";
+      Lib.current.addChild(text);
+      text.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+
+      var text = new flash.text.TextField();
+      text.x = 10 + side*120;
+      text.y = 30;
+      text.textColor = col;
+      text.htmlText = "<font size='16'>Hello !</font>";
+      Lib.current.addChild(text);
  
-   var text = new flash.text.TextField();
-   text.x = 10;
-   text.y = 50;
-   text.htmlText = "<font size='24'>Hello !</font>";
-   Lib.current.stage.addChild(text);
+      var text = new flash.text.TextField();
+      text.x = 10 + side*120;
+      text.y = 50;
+      text.textColor = col;
+      text.htmlText = "<font size='24'>Hello !</font>";
+      Lib.current.addChild(text);
 
-   var text = new flash.text.TextField();
-   text.x = 10;
-   text.y = 80;
-   text.htmlText = "<font size='36'>Hello !</font>";
-   Lib.current.stage.addChild(text);
+      var text = new flash.text.TextField();
+      text.x = 10 + side*120;
+      text.y = 80;
+      text.textColor = col;
+      text.htmlText = "<font size='36'>Hello !</font>";
+      Lib.current.addChild(text);
+   }
 }
 
 }
