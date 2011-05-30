bebef2cd3817b0d1f26a78ac1a720f6311b32e76
Hugh Sanderson <gamehaxe@gmail.com>
2011-05-30 16:26:16 +0000

    Add getAsset on nme platform too
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@662 1509560c-5e2a-0410-865c-31c25e1cfdef

--- install-tool/haxe/ApplicationMain.hx
+++ install-tool/haxe/ApplicationMain.hx
@@ -13,5 +13,24 @@ class ApplicationMain
           "::APP_ICON::"
           );
    }
+
+   public static function getAsset(inName:String):Dynamic
+   {
+      ::foreach assets::
+      if (inName=="::id::")
+      {
+         ::if (type=="image")::
+            return nme.display.BitmapData.load(inName);
+         ::elseif (type=="sound")::
+            return new nme.media.Sound(new nme.net.URLRequest(inName),null,false);
+         ::elseif (type=="music")::
+            return new nme.media.Sound(new nme.net.URLRequest(inName),null,true);
+         ::else::
+            return new nme.utils.ByteArray.load(inName);
+         ::end::
+      }
+      ::end::
+      return null;
+   }
 }
 
