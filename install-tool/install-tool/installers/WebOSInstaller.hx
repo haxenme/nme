92a86b32f067b93f7f2ef636e6fcc07fdc5e98c8
Hugh Sanderson <gamehaxe@gmail.com>
2011-07-25 14:11:19 +0000

    Add icon to webos build
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@794 1509560c-5e2a-0410-865c-31c25e1cfdef

--- install-tool/install-tool/installers/WebOSInstaller.hx
+++ install-tool/install-tool/installers/WebOSInstaller.hx
@@ -3,6 +3,7 @@ package installers;
 
 import neko.io.Path;
 import neko.Lib;
+import data.Asset;
 
 
 class WebOSInstaller extends InstallerBase {
@@ -50,6 +51,33 @@ class WebOSInstaller extends InstallerBase {
 		runCommand ("", "palm-log", [ "-f", defines.get ("APP_PACKAGE") ]);
 		
 	}
+	
+
+	override function generateContext ():Void {
+		
+		super.generateContext ();
+
+      updateIcon();
+	}
+
+   function updateIcon() {
+
+      var icon_name = icons.findIcon(64,64);
+      if (icon_name=="") {
+
+          var tmpDir = buildDirectory + "/webos/haxe";
+          mkdir(tmpDir);
+          var tmp_name = tmpDir + "/icon.png";
+          if (icons.updateIcon(64,64,tmp_name))
+             icon_name = tmp_name;
+      }
+      if (icon_name!="") {
+
+         assets.push( new data.Asset(icon_name, "icon.png", Asset.TYPE_IMAGE, "icon.png", "1") );
+         context.APP_ICON = "icon.png";
+      }
+   }
+
 	
 	
 	override function update ():Void {
