ef73e52f874aa79915930429b053e812fa83a23a
Hugh Sanderson <gamehaxe@gmail.com>
2011-07-31 15:37:08 +0000

    Move compiler setup to hxcpp
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@818 1509560c-5e2a-0410-865c-31c25e1cfdef

--- install-tool/src/installers/CPPInstaller.hx
+++ install-tool/src/installers/CPPInstaller.hx
@@ -34,61 +34,7 @@ class CPPInstaller extends DesktopInstaller {
 		
 		context.CPP_DIR = getBuildDir ();
 		
-		if (targetName == "windows") {
-			
-			setWindowsEnvironment ();
-			
-		}
-		
 	}
 	
-	
-	private function setWindowsEnvironment ():Void {
-		
-		var importProcess:Process = new Process ("cmd.exe", [ "/C", nme + "/install-tool/windows/msvc.bat" ]);
-		var foundVariables:Bool = false;
-		
-		try {
-			
-			while (true) {
-				
-				var string:String = importProcess.stdout.readLine ();
-				
-				if (string == "HXCPP_VARS") {
-					
-					foundVariables = true;
-					
-				}
-				
-				if (foundVariables) {
-					
-					if (InstallTool.verbose) {
-						
-						Lib.println (string);
-						
-					}
-					
-					var indexOfEquals:Int = string.indexOf ("=");
-					var name:String = string.substr (0, indexOfEquals);
-					
-					switch (name.toLowerCase ()) {
-						
-						case "path", "vcinstalldir", "windowssdkdir", "framework35version", "frameworkdir", "frameworkdir32", "frameworkversion", "frameworkversion32", "devenvdir", "include", "lib", "libpath":
-							
-							var value:String = string.substr (indexOfEquals + 1);
-							
-							//defines.set (name, value);
-							Sys.putEnv (name, value);
-						
-					}
-					
-				}
-				
-			}
-			
-		} catch (e:Dynamic) { };
-		
-	}
-
 
 }
