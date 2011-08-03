15a089a4079c2e7d41e9b064b44d8e3998db265b
Joshua Granick <joshua@joshuagranick.com>
2011-08-03 20:01:18 +0000

    Updated the webOS installer, fixed Mac and Linux support
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@831 1509560c-5e2a-0410-865c-31c25e1cfdef

--- install-tool/src/installers/WebOSInstaller.hx
+++ install-tool/src/installers/WebOSInstaller.hx
@@ -8,8 +8,9 @@ import data.Asset;
 
 class WebOSInstaller extends InstallerBase {
 
-   var sdkDir:String;
 	
+	private var sdkDir:String;
+   
 	
 	override function build ():Void {
 		
@@ -38,29 +39,61 @@ class WebOSInstaller extends InstallerBase {
 		runPalmCommand (true, "package" ,[ "bin", "--use-v1-format" ] );
 		
 	}
-
-   function runPalmCommand(inBinDir:Bool, inCommand:String, args:Array<String>)
-   {
-      var dir = inBinDir ? buildDirectory + "/webos" : "";
-      if (InstallTool.isWindows)
-      {
-         var jar_file = defines.get("PalmSDK") + "\\share\\jars\\webos-tools.jar";
-         var new_args = ["-Dpalm.command=palm-" + inCommand , "-jar", jar_file].concat(args);
-		   runCommand (dir, "java" , new_args );
-      }
-      else
-      {
-		   runCommand (dir, sdkDir + "/bin/palm-" + inCommand, args );
-      }
-   }
-
+	
+	
+	override function generateContext ():Void {
+		
+		sdkDir = "";
+		
+		if (defines.exists ("PalmSDK")) {
+			
+			sdkDir = defines.get ("PalmSDK");
+			
+		} else {
+			
+			if (InstallTool.isWindows) {
+				
+				sdkDir = "c:\\Program Files (x86)\\HP webOS\\SDK\\";
+				
+			} else {
+				
+				sdkDir = "/opt/PalmSDK/Current/";
+				
+			}
+			
+		}
+		
+		super.generateContext ();
+		
+		updateIcon ();
+		
+	}
 	
 	
 	override function run ():Void {
 		
 		runPalmCommand (true, "install", [ defines.get ("APP_PACKAGE") + "_" + defines.get ("APP_VERSION") + "_all.ipk" ] );
-
 		runPalmCommand (false, "launch", [ defines.get ("APP_PACKAGE") ] );
+		
+	}
+	
+	
+	private function runPalmCommand (inBinDir:Bool, inCommand:String, args:Array<String>):Void {
+		
+		var dir = inBinDir ? buildDirectory + "/webos" : "";
+		
+		if (InstallTool.isWindows) {
+			
+			var jar_file = defines.get("PalmSDK") + "\\share\\jars\\webos-tools.jar";
+			var new_args = ["-Dpalm.command=palm-" + inCommand , "-jar", jar_file].concat(args);
+			runCommand (dir, "java" , new_args );
+			
+		} else {
+			
+			runCommand (dir, sdkDir + "/bin/palm-" + inCommand, args);
+			
+		}
+		
 	}
 	
 	
@@ -69,40 +102,6 @@ class WebOSInstaller extends InstallerBase {
 		runPalmCommand (false, "log", [ "-f", defines.get ("APP_PACKAGE") ]);
 		
 	}
-	
-
-	override function generateContext ():Void {
-
-      sdkDir = "";
-
-      if (InstallTool.isWindows)
-      {
-         sdkDir = defines.exists("PalmSDK") ? defines.get("PalmSDK") : "c:\\Program Files (x86)\\HP webOS\\SDK\\";
-      }
-		
-		super.generateContext ();
-
-      updateIcon();
-	}
-
-   function updateIcon() {
-
-      var icon_name = icons.findIcon(64,64);
-      if (icon_name=="") {
-
-          var tmpDir = buildDirectory + "/webos/haxe";
-          mkdir(tmpDir);
-          var tmp_name = tmpDir + "/icon.png";
-          if (icons.updateIcon(64,64,tmp_name))
-             icon_name = tmp_name;
-      }
-      if (icon_name!="") {
-
-         assets.push( new data.Asset(icon_name, "icon.png", Asset.TYPE_IMAGE, "icon.png", "1") );
-         context.APP_ICON = "icon.png";
-      }
-   }
-
 	
 	
 	override function update ():Void {
@@ -131,6 +130,34 @@ class WebOSInstaller extends InstallerBase {
 				copyIfNewer (asset.sourcePath, destination + asset.targetPath);
 				
 			}
+			
+		}
+		
+	}
+	
+	
+	private function updateIcon ():Void {
+		
+		var icon_name = icons.findIcon (64, 64);
+		
+		if (icon_name == "") {
+			
+			var tmpDir = buildDirectory + "/webos/haxe";
+			mkdir (tmpDir);
+			var tmp_name = tmpDir + "/icon.png";
+			
+			if (icons.updateIcon (64, 64, tmp_name)) {
+				
+				icon_name = tmp_name;
+				
+			}
+			
+		}
+		
+		if (icon_name != "") {
+			
+			assets.push (new Asset (icon_name, "icon.png", Asset.TYPE_IMAGE, "icon.png", "1"));
+			context.APP_ICON = "icon.png";
 			
 		}
 		
