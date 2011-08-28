c072489a91ac2e4acaaee85d72e5bcfdbd7db993
Hugh Sanderson <gamehaxe@gmail.com>
2011-08-28 10:47:49 +0000

    Make label a member
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@863 1509560c-5e2a-0410-865c-31c25e1cfdef

--- samples/18-SharedObject/Sample.hx
+++ samples/18-SharedObject/Sample.hx
@@ -10,34 +10,35 @@ import flash.net.SharedObject;
 class Sample extends Sprite
 {
    private var so:SharedObject;
-	
+   var label:TextField;
+   
    public function new()
    {
       super();
-      var label:TextField=new TextField();
-	  label.width=600;
-	  label.height=800;
+      label=new TextField();
+      label.width=600;
+      label.height=800;
       Lib.current.stage.addChild(this);
-	  label.text="";
-	  addChild(label);
-	  label.text="Loading SO 'nmeTest':";
+      label.text="";
+      addChild(label);
+      label.text="Loading SO 'nmeTest':";
 
-	  so=SharedObject.getLocal("nmeTest");	  
+      so=SharedObject.getLocal("nmeTest");     
 
-	  var strData:String=expandAsString(so.data);
-	  trace("str data:'"+strData+"'");
-	  label.text+=strData;
-	  label.text+="\n, Click to save and flush!";
+      var strData:String=expandAsString(so.data);
+      trace("str data:'"+strData+"'");
+      label.text+=strData;
+      label.text+="\n, Click to save and flush!";
 
-	  Lib.current.stage.addEventListener(MouseEvent.CLICK,onClick);
+      Lib.current.stage.addEventListener(MouseEvent.CLICK,onClick);
    }
 
    public function onClick(inEvent:MouseEvent)
    {
-       Reflect.setField(so.data,"name","John Doe");
-	   Reflect.setField(so.data,"age","24");
-	   so.flush();
-	   label.text+="DONE";
+      Reflect.setField(so.data,"name","John Doe");
+      Reflect.setField(so.data,"age","24");
+      so.flush();
+      label.text+="DONE";
    }
 
    public static function main()
@@ -47,24 +48,24 @@ class Sample extends Sprite
    
    private static function expandAsString(obj:Dynamic):String
    {
-	   if (obj==null)
-	   {
-		   return null;
-	   }
-	   
-	   var str:String="{";
-	   var iter:Iterator<String>=Reflect.fields(obj).iterator();
-	   for (i in iter)
-	   {
-		   str+=i+"="+Reflect.field(obj,i);
-		   if (iter.hasNext())
-		   {
-			   str+=",";
-		   }
-	   }
-	   str+="}";
-	   
-	   return str;
+      if (obj==null)
+      {
+         return null;
+      }
+      
+      var str:String="{";
+      var iter:Iterator<String>=Reflect.fields(obj).iterator();
+      for (i in iter)
+      {
+         str+=i+"="+Reflect.field(obj,i);
+         if (iter.hasNext())
+         {
+            str+=",";
+         }
+      }
+      str+="}";
+      
+      return str;
    }
 
 }
