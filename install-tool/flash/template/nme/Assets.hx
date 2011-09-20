fb7281d58c8ff395e201f6d9a596457cee599a2e
Joshua Granick <joshua@joshuagranick.com>
2011-09-20 21:55:48 +0000

    Implementing a test API for asset loading
     - ApplicationMain.getAsset would still work, could be deprecated
     - Would be available at nme.Assets
     - Almost have code completion, but Haxe prefers classes from libs over any local files, unfortunately. Need to figure that part out.
    
    
    Example use:
    
    
    var bitmapData = Assets.getBitmapData ("MyImage.jpg");
    var font = Assets.getFont ("MyFont.ttf");
    var sound = Assets.getSound ("MySound.wav");
    var bytes = Assets.getBytes ("MyBytes");
    var text = Assets.getText ("MyText.txt");
    
    
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@934 1509560c-5e2a-0410-865c-31c25e1cfdef

---
+++ install-tool/flash/template/nme/Assets.hx
@@ -0,0 +1,89 @@
+package nme;
+
+
+import nme.display.BitmapData;
+import nme.media.Sound;
+import nme.net.URLRequest;
+import nme.text.Font;
+import nme.utils.ByteArray;
+import ApplicationMain;
+
+
+/**
+ * ...
+ * @author Joshua Granick
+ */
+
+class Assets {
+
+	
+	public static function getBitmapData (id:String):BitmapData {
+		
+		switch (id) {
+			
+			::foreach assets::::if (type == "image")::case "::id::": return cast (new NME_::flatName:: (), BitmapData);
+			::end::::end::
+		}
+		
+		return null;
+		
+	}
+	
+	
+	public static function getBytes (id:String):ByteArray {
+		
+		switch (id) {
+			
+			::foreach assets::case "::id::": return cast (new NME_::flatName:: (), ByteArray);
+			::end::
+		}
+		
+		return null;
+		
+	}
+	
+	
+	public static function getFont (id:String):Font {
+		
+		switch (id) {
+			
+			::foreach assets::::if (type == "font")::case "::id::": return cast (new NME_::flatName:: (), Font); 
+			::end::::end::
+		}
+		
+		return null;
+		
+	}
+	
+	
+	public static function getSound (id:String):Sound {
+		
+		switch (id) {
+			
+			::foreach assets::::if (type == "sound")::case "::id::": return cast (new NME_::flatName:: (), Sound);::elseif (type == "music")::case "::id::": return cast (new NME_::flatName:: (), Sound);
+			::end::::end::
+		}
+		
+		return null;
+		
+	}
+	
+	
+	public static function getText (id:String):String {
+		
+		var bytes:ByteArray = getBytes (id);
+		
+		if (bytes == null) {
+			
+			return null;
+			
+		} else {
+			
+			return bytes.readUTFBytes (bytes.length);
+			
+		}
+		
+	}
+	
+	
+}
