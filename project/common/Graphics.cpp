d62e908ffce677a8c1e0613696d9495bd8903aee
Joshua Granick <joshua@joshuagranick.com>
2011-12-16 21:00:13 +0000

    Fixed drawTriangles
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1157 1509560c-5e2a-0410-865c-31c25e1cfdef

--- project/common/Graphics.cpp
+++ project/common/Graphics.cpp
@@ -404,10 +404,13 @@ void Graphics::drawTriangles(const QuickVec<float> &inXYs,
             const QuickVec<int> &inColours,
             int blendMode, const QuickVec<float> &inViewport )
 {
-	if (!mTileJob.mFill)
-	{
-		beginFill (0, 0);
-	}
+	//if (!mTileJob.mFill)
+	//{
+		//beginFill (0, 0);
+	//}
+	
+	// Find a different way to allow use with vertex colors, and no beginFill?
+	
 	Flush( );
 	IGraphicsFill *fill = mFillJob.mFill;
 
