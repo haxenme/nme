07f8754af2532db5ab1c3500984565ca7fea3aad
Joshua Granick <joshua@joshuagranick.com>
2012-03-23 22:24:42 +0000

    Added Vector.ofArray for C++, Neko and JavaScript
    Currently it requires "using nme.Vector", but it should alleviate headaches with cross-platform code (like drawTriangles) that requires Vectors instead of Arrays.
    
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1464 1509560c-5e2a-0410-865c-31c25e1cfdef

--- nme/Vector.hx
+++ nme/Vector.hx
@@ -5,4 +5,15 @@ package nme;
 typedef Vector<T> = flash.Vector<T>;
 #else 
 typedef Vector<T> = Array<T>;
+
+class VectorHelper
+{
+	
+	public static inline function ofArray<T>(v:Class<Vector<Dynamic>>, array:Array<T>):Vector<T>
+	{
+		return array;
+	}
+	
+}
+
 #end
