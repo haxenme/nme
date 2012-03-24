38fc3d87a6d4575b1c94eb28598d72d7a0de4f70
Joshua Granick <joshua@joshuagranick.com>
2012-03-24 03:27:35 +0000

    Allow Lib.getURL from BlackBerry
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1465 1509560c-5e2a-0410-865c-31c25e1cfdef

--- project/common/ExternalInterface.cpp
+++ project/common/ExternalInterface.cpp
@@ -911,7 +911,7 @@ DEFINE_PRIM(nme_filesystem_get_volumes,2);
 // --- getURL ----------------------------------------------------------------------
 value nme_get_url(value url)
 {
-#if defined(HX_WINDOWS) || defined(IPHONE) || defined(ANDROID) || defined(HX_MACOS) || defined(WEBOS)
+#if defined(HX_WINDOWS) || defined(IPHONE) || defined(ANDROID) || defined(HX_MACOS) || defined(WEBOS) || defined(BLACKBERRY)
 	bool result=LaunchBrowser(val_string(url));
 	return alloc_bool(result);
 #endif
