828496b4e77e38eeeaff94d06b7dad1657f36d00
Joshua Granick <joshua@joshuagranick.com>
2012-05-22 02:03:19 +0000

    Added TILE_TRANS_2x2 to nme.display.Tilesheet
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1600 1509560c-5e2a-0410-865c-31c25e1cfdef

--- nme/display/Tilesheet.hx
+++ nme/display/Tilesheet.hx
@@ -12,9 +12,11 @@ extern class Tilesheet
 	static var TILE_ROTATION:Int;
 	static var TILE_RGB:Int;
 	static var TILE_ALPHA:Int;
+	static var TILE_TRANS_2x2:Int;
 	
 	static var TILE_BLEND_NORMAL:Int;
 	static var TILE_BLEND_ADD:Int;
+	
 	
 	function new(inImage:BitmapData):Void;
 	function addTileRect(rectangle:Rectangle, centerPoint:Point = null):Void;
@@ -58,6 +60,11 @@ class Tilesheet
 	public static inline var TILE_ROTATION = 0x0002;
 	public static inline var TILE_RGB = 0x0004;
 	public static inline var TILE_ALPHA = 0x0008;
+	
+	// Will ignore scale and rotation....
+	// (not supported right now)
+	public static inline var TILE_TRANS_2x2 = 0x0010;
+	
 	
 	public static inline var TILE_BLEND_NORMAL   = 0x00000000;
 	public static inline var TILE_BLEND_ADD      = 0x00010000;
