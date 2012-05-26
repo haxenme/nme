ed6ea0a4e461f0b81ab6b3ef353d007c2a621c24
Joshua Granick <joshua@joshuagranick.com>
2012-05-26 00:33:48 +0000

    Small documentation updates
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1608 1509560c-5e2a-0410-865c-31c25e1cfdef

--- nme/display/Bitmap.hx
+++ nme/display/Bitmap.hx
@@ -18,26 +18,6 @@ package nme.display;
  * same complex BitmapData object without incurring the memory overhead of a BitmapData
  * object for each display object instance.
  * 
- * A BitmapData object can be drawn to the screen by a Bitmap object in one of two ways:
- * by using the vector renderer as a fill-bitmap shape, or by using a faster pixel-copying routine.
- * The pixel-copying routine is substantially faster than the vector renderer, but the Bitmap object
- * must meet certain conditions to use it:
- * 
- * <ul>
- * <li> No stretching, rotation, or skewing can be applied to the Bitmap object.</li>
- * <li> No color transform can be applied to the Bitmap object. </li>
- * <li> No blend mode can be applied to the Bitmap object. </li>
- * <li> No clipping can be done through mask layers or <code>setMask()</code> methods. </li>
- * <li> The image itself cannot be a mask. </li>
- * <li> The destination coordinates must be on a whole pixel boundary. </li>
- * </ul>
- * 
- * If you load a Bitmap object from a domain other than that of the Loader object used to
- * load the image, and there is no URL policy file that permits access to the domain of
- * the Loader object, then a script in that domain cannot access the Bitmap
- * object or its properties and methods. For more information, see the Flash Player Developer Center Topic: 
- * <a href="http://www.adobe.com/go/devnet_security_en" target="external">Security</a>.</p>
- * 
  * <b>Note:</b> The Bitmap class is not a subclass of the InteractiveObject class, so
  * it cannot dispatch mouse events. However, you can use the <code>addEventListener()</code> method
  * of the display object container that contains the Bitmap object.
@@ -50,12 +30,23 @@ extern class Bitmap extends DisplayObject {
 	var bitmapData:BitmapData;
 	
 	/**
-	 * Controls whether or not the Bitmap object is snapped to the nearest pixel.
+	 * Controls whether or not the Bitmap object is snapped to the nearest pixel. The PixelSnapping
+	 * class includes possible values:
+	 * 
+	 * <ul><li><code>PixelSnapping.NEVER</code>&mdash;No pixel snapping occurs.</li>
+	 * <li><code>PixelSnapping.ALWAYS</code>&mdash;The image is always snapped to the nearest
+	 * pixel, independent of transformation.</li>
+	 * <li><code>PixelSnapping.AUTO</code>&mdash;The image is snapped
+	 * to the nearest pixel if it is drawn with no rotation
+	 * or skew and it is drawn at a scale factor of 99.9% to 100.1%. If these conditions are satisfied,
+	 * the bitmap image is drawn at 100% scale, snapped to the nearest pixel. Internally, this value allows the image
+	 * to be drawn as fast as possible.
 	 */
 	var pixelSnapping:PixelSnapping;
 	
 	/**
-	 * Controls whether or not the bitmap is smoothed when scaled.
+	 * Controls whether or not the bitmap is smoothed when scaled. If <code>true</code>, the bitmap 
+	 * is smoothed when scaled. If <code>false</code>, the bitmap is not smoothed when scaled. 
 	 */
 	var smoothing:Bool;
 	
--- nme/display/BitmapData.hx
+++ nme/display/BitmapData.hx
@@ -2,15 +2,138 @@ package nme.display;
 #if code_completion
 
 
+/**
+ * The BitmapData class lets you work with the data (pixels) of a Bitmap object. 
+ * You can use the methods of the BitmapData class
+ * to create arbitrarily sized transparent or opaque bitmap images and manipulate them in various 
+ * ways at runtime. 
+ * 
+ * The methods of the BitmapData class support 
+ * effects that are not available through the filters available to non-bitmap display objects.
+ * 
+ * A BitmapData object contains an array of pixel data. This data can represent either 
+ * a fully opaque bitmap or a transparent bitmap that contains alpha channel data. 
+ * Either type of BitmapData object is stored as a buffer of 32-bit integers. 
+ * Each 32-bit integer determines the properties of a single pixel in the bitmap.
+ * 
+ * Each 32-bit integer is a combination of four 8-bit channel values (from 0 to 255) that 
+ * describe the alpha transparency and the red, green, and blue (ARGB) values of the pixel.
+ * (For ARGB values, the most significant byte represents the alpha channel value, followed by red, 
+ * green, and blue.)
+ * 
+ * The four channels (alpha, red, green, and blue) are represented as numbers
+ * when you use them with the <code>BitmapData.copyChannel()</code> method, and these numbers
+ * are represented by the following constants in the BitmapDataChannel class:
+ * 
+ * <ul>
+ * <li>
+ * <code>BitmapDataChannel.ALPHA</code>
+ * </li>
+ * <li>
+ * <code>BitmapDataChannel.RED</code>
+ * </li>
+ * <li>
+ * <code>BitmapDataChannel.GREEN</code>
+ * </li>
+ * <li>
+ * <code>BitmapDataChannel.BLUE</code>
+ * </li>
+ * </ul>
+ * 
+ * You can attach BitmapData objects to a Bitmap object by using the 
+ * <code>bitmapData</code> property of the Bitmap object.
+ * 
+ * You can use a BitmapData object to fill a Graphics object by using the 
+ * <code>Graphics.beginBitmapFill()</code> method.
+ */
 extern class BitmapData implements IBitmapDrawable {
-	var height(default,null) : Int;
-	var rect(default,null) : nme.geom.Rectangle;
-	var transparent(default,null) : Bool;
-	var width(default,null) : Int;
-	function new(width : Int, height : Int, transparent : Bool = true, fillColor : Int = 0xFFFFFFFF) : Void;
-	function applyFilter(sourceBitmapData : BitmapData, sourceRect : nme.geom.Rectangle, destPoint : nme.geom.Point, filter : nme.filters.BitmapFilter) : Void;
-	function clone() : BitmapData;
-	function colorTransform(rect : nme.geom.Rectangle, colorTransform : nme.geom.ColorTransform) : Void;
+	
+	/**
+	 * The height of the bitmap image in pixels.
+	 */
+	var height (default, null):Int;
+	
+	/**
+	 * The rectangle that defines the size and location of the bitmap image. The 
+	 * top and left of the rectangle are 0; the width and height are equal to the 
+	 * width and height in pixels of the BitmapData object. 
+	 */
+	var rect (default, null):nme.geom.Rectangle;
+	
+	/**
+	 * Defines whether the bitmap image supports per-pixel transparency. You can set 
+	 * this value only when you construct a BitmapData object by passing in 
+	 * <code>true</code> for the transparent parameter of the <code>constructor</code>.
+	 * Then, after you create a BitmapData object, you can check whether it supports 
+	 * per-pixel transparency by determining if the value of the transparent property 
+	 * is <code>true</code>. 
+	 */
+	var transparent (default, null):Bool;
+	
+	/**
+	 * The width of the bitmap image in pixels.
+	 */
+	var width (default, null):Int;
+	
+	/**
+	 * Creates a BitmapData object with a specified width and height. If you specify a value for 
+	 * the <code>fillColor</code> parameter, every pixel in the bitmap is set to that color. 
+	 * 
+	 * By default, the bitmap is created as transparent, unless you pass the value <code>false</code>
+	 * for the transparent parameter. After you create an opaque bitmap, you cannot change it 
+	 * to a transparent bitmap. Every pixel in an opaque bitmap uses only 24 bits of color channel 
+	 * information. If you define the bitmap as transparent, every pixel uses 32 bits of color 
+	 * channel information, including an alpha transparency channel.
+	 * 
+	 * @param	width		The width of the bitmap image in pixels. 
+	 * @param	height		The height of the bitmap image in pixels. 
+	 * @param	transparent		Specifies whether the bitmap image supports per-pixel transparency. The default value is <code>true</code> (transparent). To create a fully transparent bitmap, set the value of the <code>transparent</code> parameter to <code>true</code> and the value of the <code>fillColor</code> parameter to 0x00000000 (or to 0). Setting the <code>transparent</code> property to <code>false</code> can result in minor improvements in rendering performance.
+	 * @param	fillColor		A 32-bit ARGB color value that you use to fill the bitmap image area. The default value is 0xFFFFFFFF (solid white).
+	 */
+	function new (width:Int, height:Int, transparent:Bool = true, fillColor:Int = 0xFFFFFFFF):Void;
+	
+	/**
+	 * Takes a source image and a filter object and generates the filtered image. 
+	 * 
+	 * This method relies on the behavior of built-in filter objects, which determine the 
+	 * destination rectangle that is affected by an input source rectangle.
+	 * 
+	 * After a filter is applied, the resulting image can be larger than the input image. 
+	 * For example, if you use a BlurFilter class to blur a source rectangle of (50,50,100,100) 
+	 * and a destination point of (10,10), the area that changes in the destination image is 
+	 * larger than (10,10,60,60) because of the blurring. This happens internally during the 
+	 * applyFilter() call.
+	 * 
+	 * If the <code>sourceRect</code> parameter of the sourceBitmapData parameter is an 
+	 * interior region, such as (50,50,100,100) in a 200 x 200 image, the filter uses the source 
+	 * pixels outside the <code>sourceRect</code> parameter to generate the destination rectangle.
+	 * 
+	 * If the BitmapData object and the object specified as the <code>sourceBitmapData</code> 
+	 * parameter are the same object, the application uses a temporary copy of the object to 
+	 * perform the filter. For best performance, avoid this situation.
+	 * 
+	 * @param	sourceBitmapData		The input bitmap image to use. The source image can be a different BitmapData object or it can refer to the current BitmapData instance.
+	 * @param	sourceRect		A rectangle that defines the area of the source image to use as input.
+	 * @param	destPoint		The point within the destination image (the current BitmapData instance) that corresponds to the upper-left corner of the source rectangle. 
+	 * @param	filter		The filter object that you use to perform the filtering operation. 
+	 */
+	function applyFilter (sourceBitmapData:BitmapData, sourceRect:nme.geom.Rectangle, destPoint:nme.geom.Point, filter:nme.filters.BitmapFilter):Void;
+	
+	/**
+	 * Returns a new BitmapData object that is a clone of the original instance with an exact copy of the contained bitmap. 
+	 * @return		A new BitmapData object that is identical to the original.
+	 */
+	function clone ():BitmapData;
+	
+	/**
+	 * Adjusts the color values in a specified area of a bitmap image by using a <code>ColorTransform</code>
+	 * object. If the rectangle matches the boundaries of the bitmap image, this method transforms the color 
+	 * values of the entire image. 
+	 * @param	rect		A Rectangle object that defines the area of the image in which the ColorTransform object is applied.
+	 * @param	colorTransform		A ColorTransform object that describes the color transformation values to apply.
+	 */
+	function colorTransform (rect:nme.geom.Rectangle, colorTransform:nme.geom.ColorTransform):Void;
+	
 	function compare(otherBitmapData : BitmapData) : Dynamic;
 	function copyChannel(sourceBitmapData : BitmapData, sourceRect : nme.geom.Rectangle, destPoint : nme.geom.Point, sourceChannel : Int, destChannel : Int) : Void;
 	function copyPixels(sourceBitmapData : BitmapData, sourceRect : nme.geom.Rectangle, destPoint : nme.geom.Point, ?alphaBitmapData : BitmapData, ?alphaPoint : nme.geom.Point, mergeAlpha : Bool = false) : Void;
