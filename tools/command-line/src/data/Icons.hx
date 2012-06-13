cabb1aec2892eb2dfd02604cf5ad11518958cd5f
Joshua Granick <joshua@joshuagranick.com>
2012-06-13 02:27:08 +0000

    Start on improving Mac icons
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1673 1509560c-5e2a-0410-865c-31c25e1cfdef

--- tools/command-line/src/data/Icons.hx
+++ tools/command-line/src/data/Icons.hx
@@ -1,10 +1,13 @@
 package data;
 
+
+import haxe.io.BytesOutput;
+import neko.FileSystem;
 import neko.Sys;
 import nme.display.BitmapData;
-import nme.utils.ByteArray;
 import nme.geom.Rectangle;
-import neko.FileSystem;
+import nme.utils.ByteArray;
+
 
 class Icons
 {
@@ -188,172 +191,225 @@ class Icons
 
 
 
-   public function createMacIcon(resource_dest:String)
-   {
-         var out = new haxe.io.BytesOutput();
-         out.bigEndian = true;
-         for(i in 0...2)
-         {
-            var s =  ([ 32, 48 ])[i];
-            var code =  (["il32","ih32"])[i];
-            var bmp = getIconBitmap(s,s);
-            if (bmp!=null)
-            {
-               for(c in 0...4)
-                  out.writeByte(code.charCodeAt(c));
-               var n = s*s;
-               var pixels = bmp.getPixels(new nme.geom.Rectangle(0,0,s,s));
-
-               var bytes_r = PackBits(pixels,1,s*s);
-               var bytes_g = PackBits(pixels,2,s*s);
-               var bytes_b = PackBits(pixels,3,s*s);
-
-               out.writeInt31(bytes_r.length + bytes_g.length + bytes_b.length + 8);
-               out.writeBytes(bytes_r,0,bytes_r.length);
-               out.writeBytes(bytes_g,0,bytes_g.length);
-               out.writeBytes(bytes_b,0,bytes_b.length);
-
-               var code =  (["l8mk","h8mk" ])[i];
-               for(c in 0...4)
-                  out.writeByte(code.charCodeAt(c));
-               var bytes_a = ExtractBits(pixels,0,s*s);
-               out.writeInt31(bytes_a.length + 8);
-               out.writeBytes(bytes_a,0,bytes_a.length);
-            }
-         }
-         var bytes = out.getBytes();
-         if (bytes.length>0)
-         {
-            var filename = resource_dest + "/icon.icns";
-            var file = neko.io.File.write( filename,true);
-            file.bigEndian = true;
-            for(c in 0...4)
-               file.writeByte("icns".charCodeAt(c));
-            file.writeInt31(bytes.length+8);
-            file.writeBytes(bytes,0,bytes.length);
-            file.close();
-            return filename;
-         }
- 
-      return "";
-   }
-
-   public function setWindowsIcon(inAppIcon:String, inTmp:String, inExeName:String)
-   {
-      var name:String="";
-      if (inAppIcon!=null && inAppIcon!="")
-         name = inAppIcon;
-      else
-      {
-         var sizes = [ 16, 24, 32, 48, 64, 128, 256 ];
-
-
-         var bmps = new Array<BitmapData>();
-
-         var data_pos = 6;
-         for(size in sizes)
-         {
-            var bmp = getIconBitmap(size,size);
-            if (bmp !=null) {
-				bmps.push(bmp);
-				data_pos += 16;
+	public function createMacIcon (resource_dest:String) {
+		
+		var out = new BytesOutput ();
+		out.bigEndian = true;
+		
+		for (i in 0...4) {
+			
+			var s =  ([ 16, 32, 48, 128 ])[i];
+			var code =  ([ "is32", "il32", "ih32", "it32" ])[i];
+			var bmp = getIconBitmap (s, s);
+			
+			if (bmp != null) {
+				
+				for (c in 0...4)
+					out.writeByte (code.charCodeAt(c));
+				
+				var n = s * s;
+				var pixels = bmp.getPixels (new nme.geom.Rectangle (0, 0, s, s));
+				
+				var bytes_r = PackBits (pixels, 1, s * s);
+				var bytes_g = PackBits (pixels, 2, s * s);
+				var bytes_b = PackBits (pixels, 3, s * s);
+				
+				out.writeInt31 (bytes_r.length + bytes_g.length + bytes_b.length + 8);
+				out.writeBytes (bytes_r, 0, bytes_r.length);
+				out.writeBytes (bytes_g, 0, bytes_g.length);
+				out.writeBytes (bytes_b, 0, bytes_b.length);
+				
+				var code =  ([ "s8mk", "l8mk", "h8mk", "t8mk" ])[i];
+				
+				for (c in 0...4)
+					out.writeByte (code.charCodeAt (c));
+				
+				var bytes_a = ExtractBits (pixels, 0, s * s);
+				out.writeInt31 (bytes_a.length + 8);
+				out.writeBytes (bytes_a, 0, bytes_a.length);
+				
 			}
-         }
-
-         var ico = new ByteArray();
-         ico.bigEndian = false;
-         ico.writeShort(0);
-         ico.writeShort(1);
-         ico.writeShort(bmps.length);
-
-         for(bmp in bmps)
-         {
-            var size = bmp.width;
-            var xor_size = size*size*4;
-            var and_size = size*size >> 3;
-            ico.writeByte(size);
-            ico.writeByte(size);
-            ico.writeByte(0); // palette
-            ico.writeByte(0); // reserved
-            ico.writeShort(1); // planes
-            ico.writeShort(32); // bits per pixel
-            ico.writeInt(40 + xor_size + and_size ); // Data size
-            ico.writeInt(data_pos); // Data offset
-            data_pos += 40 + xor_size + and_size;
-         }
-
-         for(bmp in bmps)
-         {
-            var size = bmp.width;
-            var xor_size = size*size*4;
-            var and_size = size*size >> 3;
-
-            ico.writeInt(40); // size (bytes)
-            ico.writeInt(size);
-            ico.writeInt(size*2);
-            ico.writeShort(1);
-            ico.writeShort(32);
-            ico.writeInt(0); // Bit fields...
-            ico.writeInt( xor_size + and_size ); // Size...
-            ico.writeInt(0); // res-x
-            ico.writeInt(0); // res-y
-            ico.writeInt(0); // cols
-            ico.writeInt(0); // important
-
-            var bits = bmp.getPixels( new nme.geom.Rectangle(0,0,size,size) );
-            var and_mask = new ByteArray();
-            for(y in 0...size)
-            {
-               var mask = 0;
-               var bit = 128;
-               bits.position = (size-1-y)*4*size;
-               for(i in 0...size)
-               {
-                  var a = bits.readByte();
-                  var r = bits.readByte();
-                  var g = bits.readByte();
-                  var b = bits.readByte();
-                  ico.writeByte(b);
-                  ico.writeByte(g);
-                  ico.writeByte(r);
-                  ico.writeByte(a);
-                  if (a<128)
-                     mask |= bit;
-                  bit = bit>>1;
-                  if (bit==0)
-                  {
-                     and_mask.writeByte(mask);
-                     bit = 128;
-                     mask = 0;
-                  }
-               }
-            }
-
-            ico.writeBytes(and_mask,0,and_mask.length);
-         }
+			
+		}
 		
-		 if (bmps.length > 0) {
-         name = inTmp + "/icon.ico";
-         var file = neko.io.File.write( name,true);
-         file.writeBytes(ico,0,ico.length);
-         file.close();
-		 } else {
-			 
-			 return;
-			 
-		 }
-      }
+		for (i in 0...3) {
+			
+			var s =  ([ 256, 512, 1024 ])[i];
+			var code =  ([ "ic08", "ic09", "ic10" ])[i];
+			var bmp = getIconBitmap (s, s);
+			
+			if (bmp != null) {
+				
+				//for (c in 0...4)
+					//out.writeByte (code.charCodeAt(c));
+				
+				// write in PNG format
+				
+				format.
+				
+			}
+			
+		}
+		
+		var bytes = out.getBytes ();
+		
+		if (bytes.length > 0) {
+			
+			var filename = resource_dest + "/icon.icns";
+			var file = neko.io.File.write (filename, true);
+			file.bigEndian = true;
+			
+			for (c in 0...4)
+				file.writeByte ("icns".charCodeAt (c));
+			
+			file.writeInt31 (bytes.length + 8);
+			file.writeBytes (bytes, 0, bytes.length);
+			file.close ();
+			
+			return filename;
+			
+		}
+		
+		return "";
+		
+	}
 	
-	  try {
+
+	public function setWindowsIcon (inAppIcon:String, inTmp:String, inExeName:String) {
 		
-		  var command = "ReplaceVistaIcon.exe";
-		InstallTool.runCommand (InstallTool.nme + "\\ndll\\Windows\\", command, [ Sys.getCwd () + "\\" + inExeName, Sys.getCwd () + "\\" + name ]);
-	  
-	  } catch (e:Dynamic) {
-		  
-	  }
-	  
-   }
+		var name:String = "";
+		
+		if (inAppIcon != null && inAppIcon != "") {
+			
+			name = inAppIcon;
+			
+		} else {
+			
+			var sizes = [ 16, 24, 32, 48, 64, 128, 256 ];
+			var bmps = new Array <BitmapData> ();
+			
+			var data_pos = 6;
+			
+			for (size in sizes) {
+				
+				var bmp = getIconBitmap(size, size);
+				
+				if (bmp != null) {
+					
+					bmps.push (bmp);
+					data_pos += 16;
+					
+				}
+				
+			}
+			
+			var ico = new ByteArray ();
+			ico.bigEndian = false;
+			ico.writeShort (0);
+			ico.writeShort (1);
+			ico.writeShort (bmps.length);
+			
+			for (bmp in bmps) {
+				
+				var size = bmp.width;
+				var xor_size = size * size * 4;
+				var and_size = size * size >> 3;
+				ico.writeByte (size);
+				ico.writeByte (size);
+				ico.writeByte (0); // palette
+				ico.writeByte (0); // reserved
+				ico.writeShort (1); // planes
+				ico.writeShort (32); // bits per pixel
+				ico.writeInt (40 + xor_size + and_size); // Data size
+				ico.writeInt (data_pos); // Data offset
+				data_pos += 40 + xor_size + and_size;
+				
+			}
+			
+			for (bmp in bmps) {
+				
+				var size = bmp.width;
+				var xor_size = size * size * 4;
+				var and_size = size * size >> 3;
+				
+				ico.writeInt (40); // size (bytes)
+				ico.writeInt (size);
+				ico.writeInt (size * 2);
+				ico.writeShort (1);
+				ico.writeShort (32);
+				ico.writeInt (0); // Bit fields...
+				ico.writeInt (xor_size + and_size); // Size...
+				ico.writeInt (0); // res-x
+				ico.writeInt (0); // res-y
+				ico.writeInt (0); // cols
+				ico.writeInt (0); // important
+				
+				var bits = bmp.getPixels (new nme.geom.Rectangle (0, 0, size, size));
+				var and_mask = new ByteArray ();
+				
+				for (y in 0...size) {
+					
+					var mask = 0;
+					var bit = 128;
+					bits.position = (size-1 - y) * 4 * size;
+					
+					for (i in 0...size) {
+						
+						var a = bits.readByte ();
+						var r = bits.readByte ();
+						var g = bits.readByte ();
+						var b = bits.readByte ();
+						ico.writeByte (b);
+						ico.writeByte (g);
+						ico.writeByte (r);
+						ico.writeByte (a);
+						
+						if (a < 128)
+							mask |= bit;
+						
+						bit = bit >> 1;
+						
+						if (bit == 0) {
+							
+							and_mask.writeByte (mask);
+							bit = 128;
+							mask = 0;
+							
+						}
+						
+					}
+					
+				}
+				
+				ico.writeBytes (and_mask, 0, and_mask.length);
+				
+			}
+			
+			if (bmps.length > 0) {
+				
+				name = inTmp + "/icon.ico";
+				var file = neko.io.File.write (name, true);
+				file.writeBytes (ico, 0, ico.length);
+				file.close ();
+				
+			} else {
+				
+				return;
+				
+			}
+			
+		}
+		
+		try {
+			
+			var command = "ReplaceVistaIcon.exe";
+			InstallTool.runCommand (InstallTool.nme + "\\ndll\\Windows\\", command, [ Sys.getCwd () + "\\" + inExeName, Sys.getCwd () + "\\" + name ]);
+			
+		} catch (e:Dynamic) {
+			
+		}
+		
+	}
 
 
 }
