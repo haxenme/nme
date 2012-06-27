ae62271e56dd6e0568c0d4626a54540d6b62c451
Patrick Krekelberg <elearningeek@gmail.com>
2012-06-27 20:48:00 +0000

    support presetting retina-ipad-size icon
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1697 1509560c-5e2a-0410-865c-31c25e1cfdef

--- tools/command-line/src/installers/IOSInstaller.hx
+++ tools/command-line/src/installers/IOSInstaller.hx
@@ -268,21 +268,19 @@ class IOSInstaller extends InstallerBase {
 	}
 	
 	private function updateIcon () {
-		var destination:String = buildDirectory + "/" + PATH + "/" + defines.get("APP_FILE") + "/";
-		mkdir (destination);
+		var destination:String = buildDirectory + "/" + PATH + "/";
+		mkdir(destination);
 		
 		var has_icon = true;
+		defines.set("PRERENDERED_ICON", "true");
 		
-		for (i in 0...4) {
-			var iname = [ "Icon.png", "Icon@2x.png", "Icon-72.png", "Icon-Small.png" ][i];
-			var size = [ 57, 114 , 72, 50 ][i];
-			
-			//var iname = [ "Icon.png", "Icon@2x.png", "Icon-72.png", "Icon-Small.png", "Icon-Small@2x.png", "Icon-Small-50.png", "iTunesArtwork" ][i];
-			//var size = [ 57, 114, 72, 29, 58, 50, 512 ][i];
+		for (i in 0...5) {
+			var iname = [ "Icon.png", "Icon@2x.png", "Icon-72.png", "Icon-72@2x.png", "Icon-Small.png" ][i];
+			var size = [ 57, 114 , 72, 144, 50 ][i];
 			
 			var name = destination + "/" + iname;
 			
-			if (!icons.updateIcon (size, size, name)) {
+			if (!icons.updateIcon(size, size, name)) {
 				has_icon = false;
 			}
 		}
