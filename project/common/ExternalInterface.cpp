417acd21b944f3f7b2743d9f17ed9aea09855a27
Joshua Granick <joshua@joshuagranick.com>
2012-07-03 21:51:18 +0000

    Reverting iOS sound changes to prevent EXC_BAD_ACCESS crash
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1721 1509560c-5e2a-0410-865c-31c25e1cfdef

--- project/common/ExternalInterface.cpp
+++ project/common/ExternalInterface.cpp
@@ -3281,6 +3281,7 @@ DEFINE_PRIM(nme_sound_from_file,2);
 
 value nme_sound_from_data(value inData, value inLen, value inForceMusic)
 {
+   #ifndef IPHONE
    int length = val_int(inLen);
    Sound *sound;
    printf("trying bytes with length %d", length);
@@ -3299,6 +3300,7 @@ value nme_sound_from_data(value inData, value inLen, value inForceMusic)
       sound->DecRef();
       return result;
    }
+   #endif
    return alloc_null();
 }
 DEFINE_PRIM(nme_sound_from_data, 3);
