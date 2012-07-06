cc6871d1864a28b1cdca26d3cc7850026bb167ac
Joshua Granick <joshua@joshuagranick.com>
2012-07-06 19:20:20 +0000

    Implement nme.Lib as a class to allow a single API from all platforms
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1746 1509560c-5e2a-0410-865c-31c25e1cfdef

--- nme/Lib.hx
+++ nme/Lib.hx
@@ -1,44 +1,210 @@
 package nme;
-#if code_completion
 
 
-extern class Lib
-{
-	static var FULLSCREEN;
-	static var BORDERLESS;
-	static var RESIZABLE;
-	static var HARDWARE;
-	static var VSYNC;
-	static var HW_AA;
-	static var HW_AA_HIRES;
+import nme.display.BitmapData;
+import nme.display.MovieClip;
+import nme.display.Stage;
+import nme.net.URLRequest;
+
+
+class Lib
+{	
 	
-	static var current(nmeGetCurrent, null):nme.display.MovieClip;
-	static var initHeight(default, null):Int;
-	static var initWidth(default, null):Int;
-	static var stage(nmeGetStage, null):nme.display.Stage;
+	public static var FULLSCREEN = 0x0001;
+	public static var BORDERLESS = 0x0002;
+	public static var RESIZABLE = 0x0004;
+	public static var HARDWARE = 0x0008;
+	public static var VSYNC = 0x0010;
+	public static var HW_AA = 0x0020;
+	public static var HW_AA_HIRES = 0x0060;
 	
-	static var company(default,null):String;
-	static var version(default,null):String;
-	static var packageName(default,null):String;
-	static var file(default,null):String;
+	public static var company(nmeGetCompany, null):String;
+	public static var current (nmeGetCurrent, null):MovieClip;
+	public static var file(nmeGetFile, null):String;
+	public static var initHeight(nmeGetInitHeight, null):Int;
+	public static var initWidth(nmeGetInitWidth, null):Int;
+	public static var packageName(nmeGetPackageName, null):String;
+	public static var stage(nmeGetStage, null):Stage;
+	public static var version(nmeGetVersion, null):String;
 	
-	static function close():Void;
-	static function create(inOnLoaded:Void->Void, inWidth:Int, inHeight:Int, inFrameRate:Float = 60.0,  inColour:Int = 0xffffff, inFlags:Int = 0x0f, inTitle:String = "NME", ?inIcon:nme.display.BitmapData):Void;
-	static function createManagedStage(inWidth:Int, inHeight:Int):nme.display.ManagedStage;
-	static function exit():Void;
-	static function forceClose():Void;
-	static function getTimer():Int;
-	static function getURL (url:nme.net.URLRequest, ?target:String):Void;
-	static function postUICallback(inCallback:Void->Void):Void;
-	static function setIcon(path:String):Void;
-	static function setPackage(inCompany:String, inFile:String, inPack:String, inVersion:String):Void;
+	
+	public static function close():Void
+	{
+		#if (cpp || neko)
+		neash.Lib.close();
+		#end
+	}
+	
+	
+	public static function create(inOnLoaded:Void->Void, inWidth:Int, inHeight:Int, inFrameRate:Float = 60.0,  inColour:Int = 0xffffff, inFlags:Int = 0x0f, inTitle:String = "NME", ?inIcon:BitmapData):Void
+	{
+		#if (cpp || neko)
+		neash.Lib.create(inOnLoaded, inWidth, inHeight, inFrameRate, inColour, inFlags, inTitle, inIcon);
+		#end
+	}
+	
+	
+	public static function createManagedStage(inWidth:Int, inHeight:Int):Void
+	{
+		#if (cpp || neko)
+		neash.Lib.createManagedStage(inWidth, inHeight);
+		#end
+	}
+	
+	
+	public static function exit():Void
+	{
+		#if (cpp || neko)
+		neash.Lib.exit();
+		#end
+	}
+	
+	
+	public static function forceClose():Void
+	{
+		#if (cpp || neko)
+		neash.Lib.forceClose();
+		#end
+	}
+	
+	
+	public inline static function getTimer():Int
+	{
+		#if (cpp || neko)
+		return neash.Lib.getTimer();
+		#elseif js
+		return jeash.Lib.getTimer();
+		#else
+		return flash.Lib.getTimer();
+		#end
+	}
+	
+	
+	public static function getURL(url:URLRequest, ?target:String):Void
+	{
+		#if (cpp || neko)
+		neash.Lib.getURL(url, target);
+		#elseif js
+		jeash.Lib.getURL(url, target);
+		#else
+		flash.Lib.getURL(url, target);
+		#end
+	}
+	
+	
+	public static function postUICallback(inCallback:Void->Void)
+	{
+		#if (cpp || neko)
+		neash.Lib.postUICallback(inCallback);
+		#end
+	}
+	
+	
+	public static function setPackage(inCompany:String, inFile:String, inPack:String, inVersion:String):Void
+	{
+		#if (cpp || neko)
+		neash.Lib.setPackage(inCompany, inFile, inPack, inVersion);
+		#end
+	}
+	
+	
+	public static function trace(arg:Dynamic):Void
+	{
+		#if (cpp || neko)
+		trace(arg);
+		#elseif js
+		jeash.Lib.trace(arg);
+		#else
+		flash.Lib.trace(arg);
+		#end
+	}
+	
+	
+	
+	
+	// Get & Set Methods
+	
+	
+	
+	private static function nmeGetCompany():String
+	{
+		#if (cpp || neko)
+		return neash.Lib.company;
+		#else
+		return "";
+		#end
+	}
+	
+	
+	private static function nmeGetCurrent ():MovieClip
+	{	
+		#if (cpp || neko)
+		return neash.Lib.current;
+		#elseif js
+		return jeash.Lib.current;
+		#else
+		return flash.Lib.current;
+		#end	
+	}
+	
+	
+	private static function nmeGetFile():String
+	{
+		#if (cpp || neko)
+		return neash.Lib.file;
+		#else
+		return "";
+		#end
+	}
+	
+	
+	private static function nmeGetInitHeight():Int
+	{
+		#if (cpp || neko)
+		return neash.Lib.initHeight;
+		#else
+		return 0;
+		#end
+	}
+	
+	
+	private static function nmeGetInitWidth():Int
+	{
+		#if (cpp || neko)
+		return neash.Lib.initWidth;
+		#else
+		return 0;
+		#end
+	}
+	
+	
+	private static function nmeGetPackageName():String
+	{
+		#if (cpp || neko)
+		return neash.Lib.packageName;
+		#else
+		return "";
+		#end
+	}
+	
+	
+	private static function nmeGetStage():Stage
+	{
+		#if (cpp || neko)
+		return neash.Lib.stage;
+		#else
+		return current.stage;
+		#end
+	}
+	
+	
+	private static function nmeGetVersion():String
+	{
+		#if (cpp || neko)
+		return neash.Lib.version;
+		#else
+		return "";
+		#end
+	}
+	
 }
-
-
-#elseif (cpp || neko)
-typedef Lib = neash.Lib;
-#elseif js
-typedef Lib = jeash.Lib;
-#else
-typedef Lib = flash.Lib;
-#end
