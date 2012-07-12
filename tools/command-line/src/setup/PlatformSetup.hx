488b103453c3b99b0ccdf2da838c6b8b276eabaf
Joshua Granick <joshua@joshuagranick.com>
2012-07-12 20:20:12 +0000

    Change permissions for ~/.android during "nme setup"
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1789 1509560c-5e2a-0410-865c-31c25e1cfdef

--- tools/command-line/src/setup/PlatformSetup.hx
+++ tools/command-line/src/setup/PlatformSetup.hx
@@ -687,8 +687,9 @@ class PlatformSetup {
 				
 			}
 			
-			if (InstallTool.isMac) {
+			if (!InstallTool.isWindows && FileSystem.exists (Sys.getEnv ("HOME") + "/.android")) {
 				
+				InstallTool.runCommand ("", "chmod", [ "-R", "777", "~/.android");
 				InstallTool.runCommand ("", "cp", [ InstallTool.nme + "/tools/command-line/bin/debug.keystore", "~/.android/debug.keystore" ] );
 				
 			}
