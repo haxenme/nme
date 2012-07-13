df706d3963668b659ca47fa09fc36e960b22973a
Joshua Granick <joshua@joshuagranick.com>
2012-07-13 01:58:38 +0000

    Fixed "tls.ndll" error on 64-bit Linux
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1791 1509560c-5e2a-0410-865c-31c25e1cfdef

--- neko/tls/Loader.hx
+++ neko/tls/Loader.hx
@@ -1,9 +1,24 @@
 package neko.tls;
 
+import sys.io.Process;
+
 class Loader {
 	
 	public static function load( f : String, args : Int ) : Dynamic {
-		return neko.Lib.load( "tls", f, args );
+		var process = new Process("uname", ["-m"]);
+		var ret = process.stdout.readAll().toString();
+		process.exitCode(); //you need this to wait till the process is closed!
+		process.close();
+		
+		if (ret.indexOf ("64") > -1) {
+				
+			return neko.Lib.load ("tls64", f, args);
+			
+		} else {
+			
+			return neko.Lib.load ("tls64", f, args);
+			
+		}
 	}
 	
 }
