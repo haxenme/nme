de75ef31e1818b1a568f47ef084985e1cd88a8ee
Joshua Granick <joshua@joshuagranick.com>
2012-07-14 00:33:33 +0000

    64-bit TLS loader fix
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1795 1509560c-5e2a-0410-865c-31c25e1cfdef

--- neko/tls/Loader.hx
+++ neko/tls/Loader.hx
@@ -5,18 +5,14 @@ import sys.io.Process;
 class Loader {
 	
 	public static function load( f : String, args : Int ) : Dynamic {
-		var process = new Process("uname", ["-m"]);
-		var ret = process.stdout.readAll().toString();
-		process.exitCode(); //you need this to wait till the process is closed!
-		process.close();
 		
-		if (ret.indexOf ("64") > -1) {
+		if (neko.Lib.load("std","sys_is64",0)()) {
 				
 			return neko.Lib.load ("tls64", f, args);
 			
 		} else {
 			
-			return neko.Lib.load ("tls64", f, args);
+			return neko.Lib.load ("tls", f, args);
 			
 		}
 	}
