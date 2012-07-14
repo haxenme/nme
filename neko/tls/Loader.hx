79ef777f84a1fead25b4071abc3f5a50cf4a5b29
Joshua Granick <joshua@joshuagranick.com>
2012-07-14 17:20:26 +0000

    Reverted Linux 64-bit patch, now that a fix is a part of the upcoming Haxe 2.10 release
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1797 1509560c-5e2a-0410-865c-31c25e1cfdef

--- neko/tls/Loader.hx
+++ neko/tls/Loader.hx
@@ -6,15 +6,8 @@ class Loader {
 	
 	public static function load( f : String, args : Int ) : Dynamic {
 		
-		if (neko.Lib.load("std","sys_is64",0)()) {
-				
-			return neko.Lib.load ("tls64", f, args);
-			
-		} else {
-			
-			return neko.Lib.load ("tls", f, args);
-			
-		}
+		return neko.Lib.load ("tls", f, args);
+		
 	}
 	
 }
