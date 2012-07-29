f9064a166731d7813260afce34c6ec0afe2b13b3
Hugh Sanderson <gamehaxe@gmail.com>
2012-07-29 15:03:27 +0000

    Remove redundant code for easier editing
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1889 1509560c-5e2a-0410-865c-31c25e1cfdef

--- project/software-renderer/SoftwareRenderer.cpp
+++ project/software-renderer/SoftwareRenderer.cpp
@@ -1,38 +1,40 @@
 #include <Graphics.h>
-#include "TriangleRender.h"
-#include "TriangleLineRender.h"
-#include "TileRenderer.h"
-#include "PointRenderer.h"
-#include "LineRender.h"
-#include "SolidRender.h"
-
+#include "PolygonRender.h"
 
 namespace nme
 {
-	
-  	Lines sLineBuffer;
-	AlphaRuns *sLines = 0;
-	Transitions *sTransitions = 0;
-	std::vector<Transitions> sTransitionsBuffer;
 
-	Renderer *Renderer::CreateSoftware(const GraphicsJob &inJob, const GraphicsPath &inPath)
-	{
-		if (inJob.mTriangles)
-		{
-			Renderer *solid = 0;
-			if (inJob.mFill)
-			 solid = new TriangleRender(inJob,inPath);
-			return inJob.mStroke ? new TriangleLineRender(inJob,inPath,solid) : solid;
-		}
-		else if (inJob.mIsTileJob)
-		  return new TileRenderer(inJob,inPath);
-		else if (inJob.mIsPointJob)
-		  return new PointRenderer(inJob,inPath);
-		else if (inJob.mStroke)
-		  return new LineRender(inJob,inPath);
-		else
-		  return new SolidRender(inJob,inPath);
-	}
+Renderer *CreateLineRenderer(const GraphicsJob &inJob, const GraphicsPath &inPath);
+Renderer *CreateTriangleLineRenderer(const GraphicsJob &inJob, const GraphicsPath &inPath, Renderer *inSolid);
+Renderer *CreateSolidRenderer(const GraphicsJob &inJob, const GraphicsPath &inPath);
+Renderer *CreatePointRenderer(const GraphicsJob &inJob, const GraphicsPath &inPath);
+Renderer *CreateTileRenderer(const GraphicsJob &inJob, const GraphicsPath &inPath);
+Renderer *CreateTriangleRenderer(const GraphicsJob &inJob, const GraphicsPath &inPath);
 
 
+Lines sLineBuffer;
+AlphaRuns *sLines = 0;
+Transitions *sTransitions = 0;
+std::vector<Transitions> sTransitionsBuffer;
+
+Renderer *Renderer::CreateSoftware(const GraphicsJob &inJob, const GraphicsPath &inPath)
+{
+   if (inJob.mTriangles)
+   {
+      Renderer *solid = 0;
+      if (inJob.mFill)
+       solid = CreateTriangleRenderer(inJob,inPath);
+      return inJob.mStroke ? CreateTriangleLineRenderer(inJob,inPath,solid) : solid;
+   }
+   else if (inJob.mIsTileJob)
+     return CreateTileRenderer(inJob,inPath);
+   else if (inJob.mIsPointJob)
+     return CreatePointRenderer(inJob,inPath);
+   else if (inJob.mStroke)
+     return CreateLineRenderer(inJob,inPath);
+   else
+     return CreateSolidRenderer(inJob,inPath);
 }
+
+
+} // end namespace nme
