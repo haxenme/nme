099e7acd6bba0518102c28d60c795934da3758f4
Joshua Granick <joshua@joshuagranick.com>
2012-08-16 15:45:07 +0000

    Small Tilesheet fix
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@1975 1509560c-5e2a-0410-865c-31c25e1cfdef

--- neash/display/Tilesheet.hx
+++ neash/display/Tilesheet.hx
@@ -14,6 +14,7 @@ class Tilesheet
 	public static inline var TILE_ROTATION = 0x0002;
 	public static inline var TILE_RGB = 0x0004;
 	public static inline var TILE_ALPHA = 0x0008;
+	public static inline var TILE_TRANS_2x2 = 0x0010;
 	
 	public static inline var TILE_BLEND_NORMAL   = 0x00000000;
 	public static inline var TILE_BLEND_ADD      = 0x00010000;
