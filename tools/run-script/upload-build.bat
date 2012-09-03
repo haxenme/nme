ff17f626e03352f935a6ff97874ff1764c2c9c87
Joshua Granick <joshua@joshuagranick.com>
2012-09-03 13:41:35 +0000

    Small Windows upload fix
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@2047 1509560c-5e2a-0410-865c-31c25e1cfdef

--- tools/run-script/upload-build.bat
+++ tools/run-script/upload-build.bat
@@ -4,9 +4,9 @@ set SCRIPT_FILE="ftp-script.txt"
 del %SCRIPT_FILE% 2>NUL
 
 echo open www.haxenme.org>> %SCRIPT_FILE%
-echo binary>> %SCRIPT_FILE%
 echo %1>> %SCRIPT_FILE%
 echo %2>> %SCRIPT_FILE%
+echo binary>> %SCRIPT_FILE%
 echo put ndll\%3 /public_html/haxenme/builds/ndll/%3>> %SCRIPT_FILE%
 echo bye>> %SCRIPT_FILE%
 
