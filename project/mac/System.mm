54c864921ac73cd4e3e38fb93f93e8f181284490
Andy Li <andy@onthewings.net>
2012-09-26 03:44:48 +0800

    Return correct dpi under OSX!

--- project/mac/System.mm
+++ project/mac/System.mm
@@ -1,6 +1,8 @@
 #import <AppKit/NSWorkspace.h>
 #import <Cocoa/Cocoa.h>
 #include <string>
+#include <sys/types.h>
+#include <sys/sysctl.h>
 
 namespace nme {
 
@@ -25,6 +27,43 @@ std::string CapabilitiesGetLanguage()
 	#endif
 	NSString *language = [[NSLocale preferredLanguages] objectAtIndex:0];
 	std::string result = (language?[language UTF8String]:"");
+	#ifndef OBJC_ARC
+	[pool drain];
+	#endif
+	return result;
+}
+
+double CapabilitiesGetScreenDPI()
+{
+	#ifndef OBJC_ARC
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	#endif
+	
+	NSScreen *screen = [NSScreen mainScreen];
+	NSDictionary *description = [screen deviceDescription];
+	NSSize displayPixelSize = [[description objectForKey:NSDeviceSize] sizeValue];
+	CGSize displayPhysicalSize = CGDisplayScreenSize(
+	            [[description objectForKey:@"NSScreenNumber"] unsignedIntValue]);
+	double result = ((displayPixelSize.width / displayPhysicalSize.width) + (displayPixelSize.height / displayPhysicalSize.height)) * 0.5 * 25.4;
+	
+	#ifndef OBJC_ARC
+	[pool drain];
+	#endif
+	return result;
+}
+
+double CapabilitiesGetPixelAspectRatio() {
+	#ifndef OBJC_ARC
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	#endif
+	
+	NSScreen *screen = [NSScreen mainScreen];
+	NSDictionary *description = [screen deviceDescription];
+	NSSize displayPixelSize = [[description objectForKey:NSDeviceSize] sizeValue];
+	CGSize displayPhysicalSize = CGDisplayScreenSize(
+	            [[description objectForKey:@"NSScreenNumber"] unsignedIntValue]);
+	double result = (displayPixelSize.width / displayPhysicalSize.width) / (displayPixelSize.height / displayPhysicalSize.height);
+	
 	#ifndef OBJC_ARC
 	[pool drain];
 	#endif
