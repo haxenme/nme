f6bacb4c1f2f0d9ef449199bc6d487d86cdd5797
Joshua Granick <joshua@joshuagranick.com>
2012-12-06 16:28:25 +0000

    Use a placeholder PNG for XFL right now. The *.dat file is not in PNG format, and needs to be read somehow
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@2227 1509560c-5e2a-0410-865c-31c25e1cfdef

--- tools/helpers/XFLHelper.hx
+++ tools/helpers/XFLHelper.hx
@@ -5,6 +5,9 @@ import format.xfl.dom.DOMBitmapItem;
 import format.XFL;
 import haxe.io.Path;
 import haxe.Serializer;
+import nme.display.BitmapData;
+import nme.geom.Rectangle;
+import nme.utils.ByteArray;
 
 
 class XFLHelper {
@@ -39,6 +42,69 @@ class XFLHelper {
 						var asset = new Asset (source, target, AssetType.IMAGE);
 						asset.id = source;
 						asset.format = (bitmapItem.isJPEG ? "jpg" : "png");
+						
+						if (!bitmapItem.isJPEG) {
+							
+							//var bytes = ByteArray.readFile (source);
+							//
+							//var format = bytes.readUnsignedByte ();
+							//var width = bytes.readUnsignedShort ();
+							//var height = bytes.readUnsignedShort ();
+							//var tableSize = bytes.readUnsignedShort () + 1;
+							//
+							//var buffer = new ByteArray ();
+							//bytes.readBytes (buffer, 0, bytes.length - bytes.position);
+							//buffer.uncompress ();
+							//
+							//var transparent = true;
+							//var colorTable = new Array <Int> ();
+							//
+							//for (i in 0...tableSize) {
+								//
+								//var r = buffer.readUnsignedByte ();
+								//var g = buffer.readUnsignedByte ();
+								//var b = buffer.readUnsignedByte ();
+								//
+								//if (transparent) {
+									//
+									//var a = buffer.readUnsignedByte ();
+									//colorTable.push ((a << 24) + (r << 16) + (g << 8) + b);
+									//
+								//} else {
+									//
+									//colorTable.push ((r << 16) + (g << 8) + b);
+									//
+								//}
+								//
+							//}
+							//
+							//var imageData = new ByteArray ();
+							//var padding = Math.ceil (width / 4) - Math.floor (width / 4);
+							//
+							//for (y in 0...height) {
+								//
+								//for (x in 0...width) {
+									//
+									//imageData.writeUnsignedInt (colorTable[buffer.readUnsignedByte ()]);
+									//
+								//}
+								//
+								//buffer.position += padding;
+								//
+							//}
+							//
+							//buffer = imageData;
+							//buffer.position = 0;
+							//
+							//var bitmapData = new BitmapData (width, height, transparent);
+							//bitmapData.setPixels (new Rectangle (0, 0, width, height), buffer);
+							
+							var bitmapData = new BitmapData (100, 100, true);
+							var png = bitmapData.encode ("png");
+							asset.data = png;
+							asset.sourcePath = "";
+							
+						}
 						
 						project.assets.push (asset);
 						
