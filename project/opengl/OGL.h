11f6d823ad32fd443d6eb523c40e621bc9939c65
Joshua Granick <joshua@joshuagranick.com>
2012-12-24 18:14:16 +0000

    Minor compile fix
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@2264 1509560c-5e2a-0410-865c-31c25e1cfdef

--- project/opengl/OGL.h
+++ project/opengl/OGL.h
@@ -15,6 +15,9 @@
 
 #include <OpenGLES/ES1/gl.h>
 #include <OpenGLES/ES1/glext.h>
+#include <OpenGLES/ES2/gl.h>
+#include <OpenGLES/ES2/glext.h>
+#define ALLOW_OGL2
 
 //typedef CAEAGLLayer *WinDC;
 //typedef EAGLContext *GLCtx;
