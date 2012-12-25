6297aa557124351e0ef66f979086a5646d16ff57
Niel Drummond <niel.drummond@gmail.com>
2012-12-25 19:40:47 +0000

    [html5] Fix one-way data binding.
    git-svn-id: https://nekonme.googlecode.com/svn/trunk@2272 1509560c-5e2a-0410-865c-31c25e1cfdef

--- browser/events/DOMEvent.hx
+++ browser/events/DOMEvent.hx
@@ -5,10 +5,10 @@ package browser.events;
 class DOMEvent extends Event {
 	
 	
-	public var domEvent:browser.Html5Dom.Event;
+	public var domEvent:browser.Html5Dom.Html5DomEvent;
 	
 	
-	public function new (type:String, domEvent:browser.Html5Dom.Event = null) {
+	public function new (type:String, domEvent:browser.Html5Dom.Html5DomEvent = null) {
 		
 		super (type, false, false);
 		
