27e855e08b24250ac48bac2b722cfa7723bfd0c3
Joshua Granick <joshua@joshuagranick.com>
2013-01-19 17:35:23 -0800

    Fix when .hxcpp_config.xml is not present

--- tools/command-line/src/CommandLineTools.hx
+++ tools/command-line/src/CommandLineTools.hx
@@ -658,7 +658,17 @@ class CommandLineTools {
 			
 			if (Sys.getEnv ("JAVA_HOME") != null) {
 				
-				config.path (PathHelper.combine (Sys.getEnv ("JAVA_HOME"), "bin"));
+				var javaPath = PathHelper.combine (Sys.getEnv ("JAVA_HOME"), "bin");
+				
+				if (PlatformHelper.hostPlatform == Platform.WINDOWS) {
+					
+					Sys.putEnv ("PATH", javaPath + ";" + Sys.getEnv ("PATH"));
+					
+				} else {
+					
+					Sys.putEnv ("PATH", javaPath + ":" + Sys.getEnv ("PATH"));
+					
+				}
 				
 			}
 			
