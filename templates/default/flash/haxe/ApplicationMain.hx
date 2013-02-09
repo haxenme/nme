35763047bb0e32a8d01082bccb17d83e64587efa
Justin Clarke <justin@digitalally.com.au>
2013-02-09 23:16:40 +1100

    Remove whitespace from ApplicationMain.hx flash template.

--- templates/default/flash/haxe/ApplicationMain.hx
+++ templates/default/flash/haxe/ApplicationMain.hx
@@ -4,38 +4,37 @@ import ::APP_MAIN_PACKAGE::::APP_MAIN_CLASS::;
 import nme.Assets;
 import nme.events.Event;
 
-
 class ApplicationMain {
-	
+
 	static var mPreloader:NMEPreloader;
 
 	public static function main () {
-		
+
 		var call_real = true;
-		
+
 		::if (PRELOADER_NAME!="")::
 		var loaded:Int = nme.Lib.current.loaderInfo.bytesLoaded;
 		var total:Int = nme.Lib.current.loaderInfo.bytesTotal;
-		
+
 		nme.Lib.current.stage.align = nme.display.StageAlign.TOP_LEFT;
 		nme.Lib.current.stage.scaleMode = nme.display.StageScaleMode.NO_SCALE;
-		
+
 		if (loaded < total || true) /* Always wait for event */ {
-			
+
 			call_real = false;
 			mPreloader = new ::PRELOADER_NAME::();
 			nme.Lib.current.addChild(mPreloader);
 			mPreloader.onInit();
 			mPreloader.onUpdate(loaded,total);
 			nme.Lib.current.addEventListener (nme.events.Event.ENTER_FRAME, onEnter);
-			
+
 		}
 		::end::
-		
+
 		#if !fdb
 		haxe.Log.trace = flashTrace;
 		#end
-		
+
 		if (call_real)
 			begin ();
 	}
@@ -44,17 +43,17 @@ class ApplicationMain {
 	private static function flashTrace( v : Dynamic, ?pos : haxe.PosInfos ) {
 		var className = pos.className.substr(pos.className.lastIndexOf('.') + 1);
 		var message = className+"::"+pos.methodName+":"+pos.lineNumber+": " + v;
-		
+
         if (flash.external.ExternalInterface.available)
 			flash.external.ExternalInterface.call("console.log", message);
 		else untyped flash.Boot.__trace(v, pos);
     }
 	#end
-	
+
 	private static function begin () {
-		
+
 		var hasMain = false;
-		
+
 		for (methodName in Type.getClassFields(::APP_MAIN::))
 		{
 			if (methodName == "main")
@@ -63,7 +62,7 @@ class ApplicationMain {
 				break;
 			}
 		}
-		
+
 		if (hasMain)
 		{
 			Reflect.callMethod (::APP_MAIN::, Reflect.field (::APP_MAIN::, "main"), []);
@@ -73,29 +72,29 @@ class ApplicationMain {
 			var instance = Type.createInstance(::APP_MAIN::, []);
 			if (Std.is (instance, nme.display.DisplayObject)) {
 				nme.Lib.current.addChild(cast instance);
-			}	
+			}
 		}
-		
+
 	}
 
 	static function onEnter (_) {
-		
+
 		var loaded = nme.Lib.current.loaderInfo.bytesLoaded;
 		var total = nme.Lib.current.loaderInfo.bytesTotal;
 		mPreloader.onUpdate(loaded,total);
-		
+
 		if (loaded >= total) {
-			
+
 			nme.Lib.current.removeEventListener(nme.events.Event.ENTER_FRAME, onEnter);
 			mPreloader.addEventListener (Event.COMPLETE, preloader_onComplete);
 			mPreloader.onLoaded();
-			
+
 		}
-		
+
 	}
 
 	public static function getAsset (inName:String):Dynamic {
-		
+
 		::foreach assets::
 		if (inName=="::id::")
 			 ::if (type=="image")::
@@ -112,23 +111,23 @@ class ApplicationMain {
             return Assets.getBytes ("::id::");
          ::end::
 		::end::
-		
+
 		return null;
-		
+
 	}
-	
-	
+
+
 	private static function preloader_onComplete (event:Event):Void {
-		
+
 		mPreloader.removeEventListener (Event.COMPLETE, preloader_onComplete);
-		
+
 		nme.Lib.current.removeChild(mPreloader);
 		mPreloader = null;
-		
+
 		begin ();
-		
+
 	}
-	
+
 }
 
 ::foreach assets::::if (type == "image")::class NME_::flatName:: extends nme.display.BitmapData { public function new () { super (0, 0); } }::else::class NME_::flatName:: extends ::flashClass:: { }::end::
@@ -139,11 +138,11 @@ class ApplicationMain {
 import ::APP_MAIN_PACKAGE::::APP_MAIN_CLASS::;
 
 class ApplicationMain {
-	
+
 	public static function main () {
-		
+
 		var hasMain = false;
-		
+
 		for (methodName in Type.getClassFields(::APP_MAIN::))
 		{
 			if (methodName == "main")
@@ -152,7 +151,7 @@ class ApplicationMain {
 				break;
 			}
 		}
-		
+
 		if (hasMain)
 		{
 			Reflect.callMethod (::APP_MAIN::, Reflect.field (::APP_MAIN::, "main"), []);
@@ -164,7 +163,7 @@ class ApplicationMain {
 				flash.Lib.current.addChild(cast instance);
 			}
 		}
-		
+
 	}
 
 }
