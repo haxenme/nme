1971181642d6cdaa00e7e58f7ed1a4e62c777961
Justin Clarke <justin@digitalally.com.au>
2013-02-10 08:26:45 +1100

    Minor formatting of ApplicationMain.hx html5 template.

--- templates/default/html5/haxe/ApplicationMain.hx
+++ templates/default/html5/haxe/ApplicationMain.hx
@@ -1,6 +1,5 @@
 #if (nme && !flambe)
 
-
 import ::APP_MAIN_PACKAGE::::APP_MAIN_CLASS::;
 import nme.display.Bitmap;
 import nme.display.Loader;
@@ -18,9 +17,7 @@ import haxe.ds.StringMap;
 typedef StringMap<T> = Hash<T>;
 #end
 
-
 class ApplicationMain {
-
 
 	private static var completed:Int;
 	private static var preloader:::PRELOADER_NAME::;
@@ -29,131 +26,97 @@ class ApplicationMain {
 	public static var loaders:StringMap <Loader>;
 	public static var urlLoaders:StringMap <URLLoader>;
 
-
-	public static function main () {
-
+	public static function main() {
 		completed = 0;
-		loaders = new StringMap <Loader> ();
-		urlLoaders = new StringMap <URLLoader> ();
+		loaders = new StringMap <Loader>();
+		urlLoaders = new StringMap <URLLoader>();
 		total = 0;
 
 		::if (WIN_WIDTH == "0")::::if (WIN_HEIGHT == "0")::
-		browser.Lib.preventDefaultTouchMove ();
+		browser.Lib.preventDefaultTouchMove();
 		::end::::end::
 
 		::if (PRELOADER_NAME!="")::
-		preloader = new ::PRELOADER_NAME:: ();
+		preloader = new ::PRELOADER_NAME::();
 		::else::
-		preloader = new NMEPreloader ();
+		preloader = new NMEPreloader();
 		::end::
-		Lib.current.addChild (preloader);
-		preloader.onInit ();
+		Lib.current.addChild(preloader);
+		preloader.onInit();
 
 		::foreach assets::
 		::if (type=="image")::
-		var loader:Loader = new Loader ();
-		loaders.set ("::resourceName::", loader);
+		var loader:Loader = new Loader();
+		loaders.set("::resourceName::", loader);
 		total ++;
 		::elseif (type == "binary")::
-		var urlLoader:URLLoader = new URLLoader ();
+		var urlLoader:URLLoader = new URLLoader();
 		urlLoader.dataFormat = BINARY;
-		urlLoaders.set ("::resourceName::", urlLoader);
+		urlLoaders.set("::resourceName::", urlLoader);
 		total ++;
 		::elseif (type == "text")::
-		var urlLoader:URLLoader = new URLLoader ();
+		var urlLoader:URLLoader = new URLLoader();
 		urlLoader.dataFormat = TEXT;
-		urlLoaders.set ("::resourceName::", urlLoader);
+		urlLoaders.set("::resourceName::", urlLoader);
 		total ++;
 		::end::::end::
 
 		if (total == 0) {
-
-			begin ();
-
+			begin();
 		} else {
-
-			for (path in loaders.keys ()) {
-
-				var loader:Loader = loaders.get (path);
-				loader.contentLoaderInfo.addEventListener ("complete", loader_onComplete);
+			for (path in loaders.keys()) {
+				var loader:Loader = loaders.get(path);
+				loader.contentLoaderInfo.addEventListener("complete",
+          loader_onComplete);
 				loader.load (new URLRequest (path));
-
 			}
 
-			for (path in urlLoaders.keys ()) {
-
-				var urlLoader:URLLoader = urlLoaders.get (path);
-				urlLoader.addEventListener ("complete", loader_onComplete);
-				urlLoader.load (new URLRequest (path));
-
+			for (path in urlLoaders.keys()) {
+				var urlLoader:URLLoader = urlLoaders.get(path);
+				urlLoader.addEventListener("complete", loader_onComplete);
+				urlLoader.load(new URLRequest (path));
 			}
-
 		}
-
 	}
 
-
-	private static function begin ():Void {
-
-		preloader.addEventListener (Event.COMPLETE, preloader_onComplete);
+	private static function begin():Void {
+		preloader.addEventListener(Event.COMPLETE, preloader_onComplete);
 		preloader.onLoaded ();
-
 	}
-
 
    public static function getAsset(inName:String):Dynamic {
-
 		::foreach assets::
 		if (inName=="::id::") {
 			::if (type == "image")::
-			return Assets.getBitmapData ("::id::");
+			return Assets.getBitmapData("::id::");
 			::elseif (type=="sound")::
-			return Assets.getSound ("::id::");
+			return Assets.getSound("::id::");
 			::elseif (type=="music")::
-			return Assets.getSound ("::id::");
+			return Assets.getSound("::id::");
 			::elseif (type== "font")::
-			return Assets.getFont ("::id::");
+			return Assets.getFont("::id::");
 			::elseif (type== "text")::
-			return Assets.getText ("::id::");
+			return Assets.getText("::id::");
 			::else::
-			return Assets.getBytes ("::id::");
+			return Assets.getBytes("::id::");
 			::end::
 		}
 		::end::
 		return null;
-
    }
 
-
-
-
-   // Event Handlers
-
-
-
-
-	private static function loader_onComplete (event:Event):Void {
-
+	private static function loader_onComplete(event:Event):Void {
 		completed ++;
-
 		preloader.onUpdate (completed, total);
-
 		if (completed == total) {
-
 			begin ();
-
 		}
-
 	}
 
-
-	private static function preloader_onComplete (event:Event):Void {
-
-		preloader.removeEventListener (Event.COMPLETE, preloader_onComplete);
-
+	private static function preloader_onComplete(event:Event):Void {
+		preloader.removeEventListener(Event.COMPLETE, preloader_onComplete);
 		Lib.current.removeChild(preloader);
 		preloader = null;
-
 		if (Reflect.field(::APP_MAIN::, "main") == null)
 		{
 			var mainDisplayObj = new ::APP_MAIN::();
@@ -162,14 +125,10 @@ class ApplicationMain {
 		}
 		else
 		{
-			Reflect.callMethod (::APP_MAIN::, Reflect.field (::APP_MAIN::, "main"), []);
+			Reflect.callMethod(::APP_MAIN::, Reflect.field (::APP_MAIN::, "main"), []);
 		}
-
 	}
-
-
 }
-
 
 ::foreach assets::
 	::if (type=="font")::
@@ -177,32 +136,19 @@ class ApplicationMain {
 	::end::
 ::end::
 
-
 #else
-
 
 import ::APP_MAIN_PACKAGE::::APP_MAIN_CLASS::;
 
-
 class ApplicationMain {
 
-
-	public static function main () {
-
+	public static function main() {
 		if (Reflect.field(::APP_MAIN::, "main") == null) {
-
-			Type.createInstance (::APP_MAIN::, []);
-
+			Type.createInstance(::APP_MAIN::, []);
 		} else {
-
-			Reflect.callMethod (::APP_MAIN::, Reflect.field (::APP_MAIN::, "main"), []);
-
+			Reflect.callMethod(::APP_MAIN::, Reflect.field(::APP_MAIN::, "main"), []);
 		}
-
 	}
-
-
 }
-
 
 #end
