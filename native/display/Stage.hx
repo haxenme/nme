01da0bcf7ab92e52fbc33f2a8c7809c6e72cd163
Joshua Granick <joshua@joshuagranick.com>
2013-02-22 08:31:21 -0800

    Update to FocusEvent bubbling (thanks gdalbec)

--- native/display/Stage.hx
+++ native/display/Stage.hx
@@ -133,41 +133,59 @@ class Stage extends DisplayObjectContainer
       nmeInvalid = true;
    }
 
-   /** @private */ private function nmeCheckFocusInOuts(inEvent:Dynamic, inStack:Array<InteractiveObject>) {
-      // Exit ...
-      var new_n = inStack.length;
-      var new_obj:InteractiveObject = new_n > 0 ? inStack[new_n - 1] : null;
-      var old_n = nmeFocusOverObjects.length;
-      var old_obj:InteractiveObject = old_n > 0 ? nmeFocusOverObjects[old_n - 1] : null;
-
-      if (new_obj != old_obj) 
-      {
-         // focusOver/focusOut goes only over the non-common objects in the tree...
-         var common = 0;
-         while(common < new_n && common < old_n && inStack[common] == nmeFocusOverObjects[common])
-            common++;
-
-         var focusOut = new FocusEvent(FocusEvent.FOCUS_OUT, false, false, new_obj, inEvent.flags > 0, inEvent.code);
-
-         var i = old_n - 1;
-         while(i >= common) 
-         {
-            nmeFocusOverObjects[i].nmeDispatchEvent(focusOut);
-            i--;
-         }
-
-         var focusIn = new FocusEvent(FocusEvent.FOCUS_IN, false, false, old_obj, inEvent.flags > 0, inEvent.code);
-         var i = new_n - 1;
-
-         while(i >= common) 
-         {
-            inStack[i].nmeDispatchEvent(focusIn);
-            i--;
-         }
-
-         nmeFocusOverObjects = inStack;
-      }
-   }
+	/** @private */ private function nmeCheckFocusInOuts(inEvent:Dynamic, inStack:Array<InteractiveObject>)
+	{
+		// Exit ...
+		var new_n = inStack.length;
+		var new_obj:InteractiveObject = new_n > 0 ? inStack[new_n - 1] : null;
+		var old_n = nmeFocusOverObjects.length;
+		var old_obj:InteractiveObject = old_n > 0 ? nmeFocusOverObjects[old_n - 1] : null;
+		
+		//if (new_obj != old_obj) 
+		//{
+			// focusOver/focusOut goes only over the non-common objects in the tree...
+			//var common = 0;
+			//while(common < new_n && common < old_n && inStack[common] == nmeFocusOverObjects[common])
+				//common++;
+			//
+			//var focusOut = new FocusEvent(FocusEvent.FOCUS_OUT, false, false, new_obj, inEvent.flags > 0, inEvent.code);
+			//
+			//var i = old_n - 1;
+			//while(i >= common) 
+			//{
+				//nmeFocusOverObjects[i].nmeDispatchEvent(focusOut);
+				//i--;
+			//}
+			//
+			//var focusIn = new FocusEvent(FocusEvent.FOCUS_IN, false, false, old_obj, inEvent.flags > 0, inEvent.code);
+			//var i = new_n - 1;
+			//
+			//while(i >= common) 
+			//{
+				//inStack[i].nmeDispatchEvent(focusIn);
+				//i--;
+			//}
+			//
+			//nmeFocusOverObjects = inStack;
+		//}
+		
+		if (new_obj != old_obj)
+		{
+			if (old_obj != null)
+			{
+				var focusOut = new FocusEvent(FocusEvent.FOCUS_OUT, true, false, new_obj, inEvent.flags > 0, inEvent.code);
+				focusOut.target = old_obj;
+				old_obj.nmeFireEvent(focusOut);
+			}
+			
+			var focusIn = new FocusEvent(FocusEvent.FOCUS_IN, true, false, old_obj, inEvent.flags > 0, inEvent.code);
+			
+			focusIn.target = new_obj;
+			new_obj.nmeFireEvent(focusIn);
+			
+			nmeFocusOverObjects = inStack;
+		}
+	}
 
    /** @private */ private function nmeCheckInOuts(inEvent:MouseEvent, inStack:Array<InteractiveObject>, ?touchInfo:TouchInfo) {
       var prev = touchInfo == null ? nmeMouseOverObjects : touchInfo.touchOverObjects;
