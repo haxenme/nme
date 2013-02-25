4288fcc6957356800c283b6c99d6dd8a9bc34c70
Joshua Granick <joshua@joshuagranick.com>
2013-02-25 15:46:22 -0800

    Have to switch back to basic typedefs for now on the Vector type, because properties must be "(get, never)"
    Once abstract types can accept setters, we can switch it back, and could also use abstract types to help resolve some other areas of the API, such as extending flash.display.MovieClip

--- nme/Vector.hx
+++ nme/Vector.hx
@@ -2,22 +2,158 @@ package nme;
 
 
 #if flash
-private typedef VectorData<T> = flash.Vector<T>;
+typedef Vector<T> = flash.Vector<T>;
 #else
-private typedef VectorData<T> = Array<T>;
+typedef Vector<T> = Array<T>;
 #end
 
-
-@:arrayAccess abstract Vector<T>(VectorData<T>) {
-	
-	@:from static public inline function fromArray(a:Array<T>) {
-		
-        #if flash
-		return new flash.Vector<T> (a);
-		#else
-		return cast a;
-		#end
-		
-    }
-	
-}
+//#if flash
+//private typedef VectorData<T> = flash.Vector<T>;
+//#else
+//private typedef VectorData<T> = Array<T>;
+//#end
+//
+//
+//@:arrayAccess abstract Vector<T>(VectorData<T>) {
+	//
+	//
+	//public var length(get, never):Int;
+	//public var fixed(get, never):Bool;
+	//
+	//
+	//public function new(?length:Int, ?fixed:Bool):Void {
+		//
+		//#if flash
+		//this = new flash.Vector<T>(length, fixed);
+		//#else
+		//this = new Array<T>();
+		//#end
+		//
+	//}
+	//
+	//
+	//public function concat(?a:VectorData<T>):VectorData<T> {
+		//
+		//return this.concat(a);
+		//
+	//}
+	//
+	//
+	//public function join(sep:String):String {
+		//
+		//return this.join(sep);
+		//
+	//}
+	//
+	//
+	//public function pop():Null<T> {
+		//
+		//return this.pop();
+		//
+	//}
+	//
+	//
+	//public function push(x:T):Int {
+		//
+		//return this.push(x);
+		//
+	//}
+	//
+	//
+	//public function reverse():Void {
+		//
+		//this.reverse();
+		//
+	//}
+	//
+	//
+	//public function shift():Null<T> {
+		//
+		//return this.shift();
+		//
+	//}
+	//
+	//
+	//public function unshift(x:T):Void {
+		//
+		//this.unshift(x);
+		//
+	//}
+	//
+	//
+	//public function slice(pos:Int, ?end:Int):VectorData<T> {
+		//
+		//return this.slice(pos, end);
+		//
+	//}
+	//
+	//
+	//public function sort(f:T -> T -> Int):Void {
+		//
+		//this.sort(f);
+		//
+	//}
+	//
+	//
+	//public function splice(pos:Int, len:Int):VectorData<T> {
+		//
+		//return this.splice(pos, len);
+		//
+	//}
+	//
+	//
+	//public function toString():String {
+		//
+		//return this.toString();
+		//
+	//}
+	//
+	//
+	//public function indexOf(x:T, ?from:Int):Int {
+		//
+		//return this.indexOf(x, from);
+		//
+	//}
+	//
+	//
+	//public function lastIndexOf(x:T, ?from:Int):Int {
+		//
+		//return this.lastIndexOf(x, from);
+		//
+	//}
+	//
+	//
+	//public inline static function ofArray<T>(a:Array<T>):VectorData<T> {
+		//
+		//#if flash
+		//return flash.Vector.ofArray (a);
+		//#else
+		//return cast a;
+		//#end
+		//
+	//}
+	//
+	//
+	//public inline static function convert<T,U>(v:VectorData<T>):VectorData<U> {
+		//
+		//#if flash
+		//return flash.Vector.convert (v);
+		//#else
+		//return cast v;
+		//#end
+		//
+	//}
+	//
+	//
+	//@:from static public inline function fromArray(a:Array<T>) {
+		//
+        //#if flash
+		//return cast flash.Vector.ofArray (a);
+		//#else
+		//return cast a;
+		//#end
+		//
+    //}
+	//
+	//
+//}
