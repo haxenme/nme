d9be3e43e8c9e73101bb0bd6a34f66add7fe32a7
Joshua Granick <joshua@joshuagranick.com>
2013-04-28 14:47:05 -0700

    Minor memory buffer fix

--- native/utils/ArrayBufferView.hx
+++ native/utils/ArrayBufferView.hx
@@ -121,14 +121,14 @@ class ArrayBufferView implements IMemoryRange
    inline public function getInt16(bytePos:Int):Int 
    {
       #if cpp
-      untyped return __global__.__hxcpp_memory_get_ui16(bytes, bytePos + byteOffset);
+      untyped return __global__.__hxcpp_memory_get_i16(bytes, bytePos + byteOffset);
       #else
       buffer.position = bytePos + byteOffset;
       return buffer.readShort();
       #end
    }
 
-   inline public function setInt16(bytePos:Int, v:Float):Void 
+   inline public function setInt16(bytePos:Int, v:Int):Void 
    {
       #if cpp
       untyped __global__.__hxcpp_memory_set_i16(bytes, bytePos + byteOffset, v);
