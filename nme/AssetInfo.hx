5ce156fca3f28772deccda5c8ca27ce77a0faa7b
hughsando <git@nmehost>
2013-11-30 22:23:47 +0800

    Add AssetInfo class

---
+++ nme/AssetInfo.hx
@@ -0,0 +1,35 @@
+package nme;
+
+import nme.utils.WeakRef;
+
+class AssetInfo
+{
+   public var path:String;
+   public var className:String;
+   public var type:AssetType;
+   public var cache:WeakRef<Dynamic>;
+
+   public static function new(inPath:String, inType:AssetType, ?inClassName:String)
+   {
+      path = inPath;
+      type = inType;
+      className = inClassName;
+   }
+
+   public function getCache() : Dynamic
+   {
+      if (cache==null)
+         return null;
+      var val = cache.get();
+      if (val==null)
+         cache = null;
+      return val;
+   }
+
+   public function setCache(inVal:Dynamic, inWeak:Bool)
+   {
+      cache = new WeakRef<Dynamic>(inVal,inWeak);
+   }
+}
+
+
