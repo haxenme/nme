bb00b96c5128d7c17073ad6df7287c2d6d9531ee
hughsando <gamehaxe@gmail.com>
2014-06-01 23:01:47 +0800

    Add some gl helpers

---
+++ nme/gl/Buffer.hx
@@ -0,0 +1,38 @@
+package nme.gl;
+
+import nme.utils.Float32Array;
+
+class Buffer
+{
+   var buffer:GLBuffer;
+   var slot:Int;
+   var dims:Int;
+
+   function new(inProgram:GLProgram, inName:String, inDims:Int, inValues:Float32Array)
+   {
+      buffer = GL.createBuffer();
+      slot =  GL.getAttribLocation(inProgram, inName);
+      dims = inDims;
+      GL.bindBuffer(GL.ARRAY_BUFFER, buffer);
+      GL.bufferData(GL.ARRAY_BUFFER, inValues, GL.STATIC_DRAW);
+      GL.bindBuffer(GL.ARRAY_BUFFER, null);
+   }
+
+   public function bind()
+   {
+      GL.bindBuffer(GL.ARRAY_BUFFER, buffer);
+      GL.vertexAttribPointer(slot, dims, GL.FLOAT, false, 0, 0);
+      GL.enableVertexAttribArray(slot);
+   }
+
+   public function unbind()
+   {
+      GL.disableVertexAttribArray(slot);
+   }
+
+   public static function fromArray(inProgram:GLProgram, inName:String, inComponents:Int, inValues:Array<Float>)
+   {
+       return new Buffer(inProgram, inName, inComponents, new Float32Array(inValues) );
+   }
+}
+
---
+++ nme/gl/Utils.hx
@@ -0,0 +1,39 @@
+package nme.gl;
+
+
+class Utils
+{
+   public static function createShader(source:String, type:Int)
+   {
+      var shader = GL.createShader(type);
+      GL.shaderSource(shader, source);
+      GL.compileShader(shader);
+      if (GL.getShaderParameter(shader, GL.COMPILE_STATUS)==0)
+      {
+         trace("--- ERR ---\n" + source);
+         var err = GL.getShaderInfoLog(shader);
+         if (err!="")
+            throw err;
+      }
+      return shader;
+   }
+
+   public static function createProgram(inVertexSource:String, inFragmentSource:String)
+   {
+      var program = GL.createProgram();
+      var vshader = createShader(inVertexSource, GL.VERTEX_SHADER);
+      var fshader = createShader(inFragmentSource, GL.FRAGMENT_SHADER);
+      GL.attachShader(program, vshader);
+      GL.attachShader(program, fshader);
+      GL.linkProgram(program);
+      if (GL.getProgramParameter(program, GL.LINK_STATUS)==0)
+      {
+         var result = GL.getProgramInfoLog(program);
+         if (result!="")
+            throw result;
+      }
+
+      return program;
+   }
+}
+
