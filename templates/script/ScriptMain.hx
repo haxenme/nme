7e087f1e23d268a9bcf6dca6f972544c043a245b
hughsando <gamehaxe@gmail.com>
2014-07-24 23:55:14 +0800

    Set window attribute from sciptable

--- templates/script/ScriptMain.hx
+++ templates/script/ScriptMain.hx
@@ -5,6 +5,15 @@ class ScriptMain
       #if nme
          nme.app.Application.setPackage("::APP_COMPANY::", "::APP_FILE::", "::APP_PACKAGE::", "::APP_VERSION::");
 
+         var stage = nme.Lib.current.stage;
+         stage.frameRate = ::WIN_FPS::;
+         stage.opaqueBackground = ::WIN_BACKGROUND::;
+         nme.app.Application.initWidth = ::WIN_WIDTH::;
+         nme.app.Application.initHeight = ::WIN_HEIGHT::;
+
+         stage.resize(::WIN_WIDTH::,::WIN_HEIGHT::);
+           //icon  : Assets.info.get("::WIN_ICON::")==null ? null : getAsset("::WIN_ICON::")
+
 
          nme.app.Application.setFixedOrientation(
             ::if (WIN_ORIENTATION == "portrait")::
