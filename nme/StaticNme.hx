c3c17e866adb93a756cb2eac19de2029173b2890
Hugh <gamehaxe@gmail.com>
2015-08-12 23:03:08 +0800

    Work on alternate static-linking method

--- nme/StaticNme.hx
+++ nme/StaticNme.hx
@@ -1,12 +1,22 @@
 package nme;
 
 @:cppFileCode( 'extern "C" void nme_register_prims();')
+#if new_link
+@:buildXml("
+<set name='NME_STATIC_LINK' value='1' />
+<import name='${haxelib:nme}/project/Build.xml'/>
+<target id='haxe'>
+  <merge id='nme-target'/>
+</target>
+")
+#else
 @:buildXml("
 <target id='haxe'>
   <lib name='${haxelib:nme}/lib/${BINDIR}/libnme{MSVC_VER}${LIBEXTRA}${LIBEXT}'/>
 </target>
 <include name='${haxelib:nme}/lib/NmeLink.xml'/>
 ")
+#end
 @:keep class StaticNme
 {
    static function __init__()
