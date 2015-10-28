5e4e5e1aec77af0efaa3fdeb40ed28ab0f5be1a0
Thomas Uster <thomasuster@gmail.com>
2015-10-27 21:03:28 -0700

    Convert travis to OSX and add error and stdout if output exists.

--- tools/nme/test/nmml/TestNMMLParser.hx
+++ tools/nme/test/nmml/TestNMMLParser.hx
@@ -6,6 +6,12 @@ class TestNMMLParser extends haxe.unit.TestCase
         var previous:String = Sys.getCwd();
         Sys.setCwd('test/nmml/renameExample');
         var process:Process = new Process('nme', ['test']);
+        var all:String = process.stdout.readAll().toString();
+        if(all != '')
+            Sys.println(all);
+        var error:String = process.stderr.readAll().toString();
+        if(error != '')
+            Sys.println(error);
         Sys.setCwd(previous);
         assertTrue(process.exitCode() == 0);
     }
