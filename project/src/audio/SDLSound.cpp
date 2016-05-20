33d2d010599f9d60e8e7a0b9c8081929d6a1e48f
hughsando <gamehaxe@gmail.com>
2016-05-20 20:47:42 +0800

    Use common code to inititialize SDL sounds

--- project/src/audio/SDLSound.cpp
+++ project/src/audio/SDLSound.cpp
@@ -645,16 +645,8 @@ class SDLSound : public Sound
 public:
    SDLSound(const std::string &inFilename)
    {
-      IncRef();
+      initSound();
       filename = inFilename;
-      mChunk = 0;
-      loaded = false;
-      frequency = 0;
-      format = 0;
-      channels = 0;
-      duration = 0.0;
-      soundData = 0;
-
       Mix_QuerySpec(&frequency, &format, &channels);
 
       if (Init())
@@ -663,13 +655,25 @@ public:
 
    SDLSound(const unsigned char *inData, int len)
    {
-      loaded = false;
-      IncRef();
+      initSound();
       if (Init())
       {
          mChunk = Mix_LoadWAV_RW(SDL_RWFromConstMem(inData, len), 1);
          onChunk();
       }
+   }
+
+   void initSound()
+   {
+      IncRef();
+      mChunk = 0;
+      loaded = false;
+      frequency = 0;
+      format = 0;
+      channels = 0;
+      duration = 0.0;
+      soundData = 0;
+      Mix_QuerySpec(&frequency, &format, &channels);
    }
 
    ~SDLSound()
