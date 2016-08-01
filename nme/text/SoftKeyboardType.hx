b0682bd7ba0cc5f209ce73d55ccef382e6b556cd
codeservice <codeservice@gmail.com>
2016-08-01 17:30:45 -0400

    String to Int
    Found my answer. Flash SoftKeyboardType for Adobe Air, NME does not support it.
    I'll update my commit

--- nme/text/SoftKeyboardType.hx
+++ nme/text/SoftKeyboardType.hx
@@ -4,12 +4,15 @@ package nme.text;
 @:nativeProperty
 class SoftKeyboardType
 {
-    public static inline var CONTACT : String = "contact";//A keypad designed for entering a person's name or phone number.
-    public static inline var DEFAULT : String = "default";//Default keyboard for the current input method.
-    public static inline var EMAIL : String = "email";//A keyboard optimized for specifying email addresses.
-    public static inline var NUMBER : String = "number";//A numeric keypad designed for PIN entry.
-    public static inline var PUNCTUATION : String = "punctuation";//A numeric keypad designed for PIN entry.
-    public static inline var URL : String = "url";//A keyboard optimized for entering URLs.
+    public static inline var DEFAULT : Int = 0;//Default keyboard for the current input method.
+    public static inline var CONTACT : Int = 1;//A keypad designed for entering a person's name or phone number.
+    public static inline var EMAIL : Int = 2;//A keyboard optimized for specifying email addresses.
+    public static inline var NUMBER : Int = 3;//A numeric keypad designed for PIN entry.
+    public static inline var PUNCTUATION : Int = 4;//A numeric keypad designed for PIN entry.
+    public static inline var URL : Int = 5;//A keyboard optimized for entering URLs.
+
+    public static inline var ANDROID_VISIBLE_PASSWORD: Int = 101;
+    public static inline var IOS_ASCII: Int = 102;
 
     public function new()
     {
