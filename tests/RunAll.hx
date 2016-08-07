3189a4cebb4b77bd57e5332839c06ed48f09f5c0
Hugh <gamehaxe@gmail.com>
2016-08-08 00:23:57 +0800

    Exclude watchos from tests

--- tests/RunAll.hx
+++ tests/RunAll.hx
@@ -25,7 +25,7 @@ class RunAll
 
    static function filter(inName:String):Bool
    {
-      return inName!="android" && inName!="native" && inName!="html5" && inName!="SwfAssetLib.hx";
+      return inName!="android" && inName!="native" && inName!="html5" && inName!="watchos" && inName!="SwfAssetLib.hx";
    }
 
    public static function main()
