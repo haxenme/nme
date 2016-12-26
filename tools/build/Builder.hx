cb306bfa840a63975989de208b6232be9a795325
hughsando <gamehaxe@gmail.com>
2016-12-26 21:45:11 +0800

    Add option for -Dnme-dev to build using nme-dev

--- tools/build/Builder.hx
+++ tools/build/Builder.hx
@@ -1,14 +1,23 @@
 
 class Builder extends hxcpp.Builder
 {
+   static var toolkitBuild = true;
+
    override public function getBuildFile()
    {
-      return "ToolkitBuild.xml";
+      if (toolkitBuild)
+         return "ToolkitBuild.xml";
+      else
+         return "Build.xml";
    }
 
    public static function main()
    {
-      new Builder( Sys.args() );
+      var args = Sys.args();
+      if (args.remove("-Dnme-dev"))
+         toolkitBuild = false;
+
+      new Builder( args );
    }
 }
 
