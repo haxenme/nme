67b868a34c64685be9812e438c0ad19386a46d5b
hughsando <gamehaxe@gmail.com>
2017-04-24 20:45:08 +0800

    Ignore compat for testing

--- tests/RunAll.hx
+++ tests/RunAll.hx
@@ -25,7 +25,7 @@ class RunAll
 
    static function filter(inName:String):Bool
    {
-      return inName!="android" && inName!="native" && inName!="html5" && inName!="watchos" && inName!="SwfAssetLib.hx" && inName!="ios";
+      return inName!="android" && inName!="native" && inName!="html5" && inName!="watchos" && inName!="SwfAssetLib.hx" && inName!="ios" && inName!="compat";
    }
 
    public static function main()
