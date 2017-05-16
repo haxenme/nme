6ecaba4841a128a5263aa1606c87a8f399dfdf27
hughsando <gamehaxe@gmail.com>
2017-05-16 22:18:01 +0800

    Start on html preloader

---
+++ nme/preloader/HtmlPreloader.hx
@@ -0,0 +1,36 @@
+package nme.preloader;
+import js.html.*;
+
+class HtmlPreloader
+{
+   static var context:CanvasRenderingContext2D;
+   static var canvas:CanvasElement;
+
+   public static function render(count:Int, of:Int)
+   {
+      var w = canvas.width;
+      var h = canvas.height;
+      var ctx = context;
+      ctx.fillStyle = "#b0b0b0";
+      ctx.fillRect(0,0,w,h);
+      ctx.strokeStyle = "#000060";
+      ctx.lineWidth = 1;
+      ctx.beginPath();
+      ctx.rect(w*0.2, h/2-30,w*0.6, 60);
+      ctx.stroke();
+      ctx.fillStyle = "#000060";
+      ctx.fillRect(w*0.22, h/2-28,w*0.56*(count+1)/(of+1), 56);
+   }
+
+   public static function main()
+   {
+      var doc  = js.Browser.window.document;
+      canvas = cast doc.createElement("canvas");
+      canvas.width = js.Browser.window.innerWidth;
+      canvas.height = js.Browser.window.innerHeight;
+      doc.body.appendChild(canvas);
+      context = canvas.getContext2d();
+      render(0,10);
+      untyped js.Browser.window.preloadUpdate = render;
+   }
+}
