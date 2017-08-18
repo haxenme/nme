0323ba84da0db954927ec58ebe64f044aa0d63f6
Carlos Madrazo <madrazo@bamtang.com>
2017-08-17 23:19:17 -0500

    added default template
    for command:
    nme create project "com.package.name" "Company Name"
    
    fixes https://github.com/haxenme/nme/issues/488
    
    Based on DisplayingABitmap

---
+++ templates/default/project/Source/Main.hx
@@ -0,0 +1,17 @@
+import nme.display.Bitmap;
+import nme.display.Sprite;
+import nme.Assets;
+import nme.Lib;
+
+
+class Main extends Sprite {
+	
+	
+	public function new () {
+		
+		super ();
+		
+	}
+	
+	
+}
