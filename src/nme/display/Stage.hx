c2642c17c588cf86ac9eeec1f24c029b61947e12
Carlos Madrazo <madrazo@bamtang.com>
2017-09-21 01:22:33 -0500

    Optimize Joystick
    - GameController API intead of Joystick API to fix button mapping. Added
    extra joystick database for older joysticks.
    - Send event values directly from cpp avoiding comparing strings and
    avoided creating arrays.
    A struct controllerState is used.
    - Added HandlingJoystickEvents sample. Includes skeleton code to manage
    buttons from joystick events.
    - Get SDL game controller index in joystick event, named "user" (see
    HandlingJoystickEvents ).
    - BallMove event is currently not used but not removed for
    retrocompatibility. Same for z and w axis.
    
    TODOs:
    Fallback to the joystick api?
    Haptics
    "axis" array for retrocompatibility?

--- src/nme/display/Stage.hx
+++ src/nme/display/Stage.hx
@@ -111,7 +111,7 @@ class Stage extends DisplayObjectContainer implements nme.app.IPollClient implem
 
    public static var nmeQuitting = false;
 
-   private var nmeJoyAxisData:Map<Int,Array <Float>>;
+   //private var nmeJoyAxisData:Map<Int,Array <Float>>;
    private var nmeDragBounds:Rectangle;
    private var nmeDragObject:Sprite;
    private var nmeDragOffsetX:Float;
@@ -166,7 +166,7 @@ class Stage extends DisplayObjectContainer implements nme.app.IPollClient implem
       nmeLastDown = [];
       nmeLastClickTime = 0.0;
       nmeTouchInfo = new Map<Int,TouchInfo>();
-      nmeJoyAxisData = new Map<Int,Array<Float>>();
+      //nmeJoyAxisData = new Map<Int,Array<Float>>();
 
       #if stage3d
       stage3Ds = new Vector();
@@ -676,51 +676,7 @@ class Stage extends DisplayObjectContainer implements nme.app.IPollClient implem
 
    public function onJoystick(inEvent:AppEvent, inType:String):Void
    {
-      var evt:JoystickEvent = null;
-
-      switch(inType) 
-      {
-         case JoystickEvent.AXIS_MOVE:
-            var data = nmeJoyAxisData.get(inEvent.id);
-            if (data == null) 
-               data = [ 0.0, 0.0, 0.0, 0.0 ];
-
-            var value:Float = inEvent.value / 32767; // Range: -32768 to 32767
-            if (value < -1) value = -1;
-
-            while(data.length < inEvent.code) 
-               data.push(0);
-
-            data[inEvent.code] = value;
-
-            evt = new JoystickEvent(inType, false, false, inEvent.id, 0, data[0], data[1], data[2], data[3]);
-            evt.axis = data.copy();
-
-            nmeJoyAxisData.set(inEvent.id, data);
-
-         case JoystickEvent.BALL_MOVE:
-            evt = new JoystickEvent(inType, false, false, inEvent.id,  inEvent.code, inEvent.x, inEvent.y);
-
-         case JoystickEvent.HAT_MOVE:
-            var x = 0;
-            var y = 0;
-
-            if (inEvent.value & 0x01 != 0) 
-               y = -1; // up
-            else if (inEvent.value & 0x04 != 0) 
-               y = 1; // down
-
-            if (inEvent.value & 0x02 != 0) 
-               x = 1; // right
-            else if (inEvent.value & 0x08 != 0) 
-               x = -1; // left
-
-            evt = new JoystickEvent(inType, false, false, inEvent.id, inEvent.code, x, y);
-
-         default:
-            evt = new JoystickEvent(inType, false, false, inEvent.id, inEvent.code);
-      }
-
+      var evt:JoystickEvent = new JoystickEvent(inType, false, false, inEvent.id, inEvent.code, inEvent.value, inEvent.sx, inEvent.sy);
       nmeDispatchEvent(evt);
    }
 
