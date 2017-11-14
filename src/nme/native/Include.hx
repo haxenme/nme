c992609e28b884ee5b3936b0a2526847852c47dc
hughsando <gamehaxe@gmail.com>
2017-11-14 21:47:08 +0800

    Add classPath to haxelib.json file

--- src/nme/native/Include.hx
+++ src/nme/native/Include.hx
@@ -3,6 +3,7 @@ package nme.native;
 @:buildXml("
    <files id='haxe'>
       <compilerflag value='-I${haxelib:nme}/include'/>
+      <compilerflag value='-I${haxelib:nme}/../include'/>
       <compilerflag value='-DHX_UNDEFINE_H'/>
    </files>
 ")
