731d35fa1dc50ff4357e25191cfe7aa46a99099c
Skuse <git@skuse.co.uk>
2018-08-02 18:57:38 +0100

    keep android screen on

--- templates/android/PROJ/src/org/haxe/nme/GameActivity.java
+++ templates/android/PROJ/src/org/haxe/nme/GameActivity.java
@@ -150,6 +150,7 @@ implements SensorEventListener
       mAssets = getAssets();
       requestWindowFeature(Window.FEATURE_NO_TITLE);
       getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
       ::end::
 
       _sound = new Sound(mContext);
