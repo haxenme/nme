d3ae8543d749b5976fc6830a1814cfd2cdce627c
Carlos Madrazo <madrazo@bamtang.com>
2019-11-19 23:33:14 -0500

    Update gamecontroller db, allow user mappings
    update game controller database,
    work around large string size limits,
    add mappings for ios android

--- project/src/sdl2/SDL2Stage.cpp
+++ project/src/sdl2/SDL2Stage.cpp
@@ -61,8 +61,11 @@ int InitSDL()
    if (err == 0 && SDL_InitSubSystem (SDL_INIT_GAMECONTROLLER) == 0)
    {
       sgGameControllerEnabled = true;
-      #if defined(HX_WINDOWS) || defined(HX_MACOS) || defined(HX_LINUX) 
-      SDL_GameControllerAddMappingsFromRW (SDL_RWFromConstMem (g_gameControllerDB, sizeof (g_gameControllerDB)), 0);
+      #if defined(HX_WINDOWS) || defined(HX_MACOS) || defined(HX_LINUX) || defined(ANDROID) || defined(IPHONE) 
+      for(int i=0; i<g_nGameControllerDBs; i++)
+            SDL_GameControllerAddMappingsFromRW (SDL_RWFromConstMem (g_gameControllerDB[i], sizeof (g_gameControllerDB[i])), 0);
+
+	  SDL_GameControllerAddMappingsFromFile("gamecontrollerdb.txt"); //user defined mappings
       #endif
    }
    
