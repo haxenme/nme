036f687420b6a2091ed89d2619b6d032feb72ee4
Hugh Sanderson <gamehaxe@gmail.com>
2024-06-08 16:33:20 +0800

    Fix ogg include path for android

--- project/toolkit/ogg/config_types.h
+++ project/toolkit/ogg/config_types.h
@@ -21,6 +21,7 @@ typedef unsigned short ogg_uint16_t;
 typedef int32_t ogg_int32_t;
 typedef unsigned int ogg_uint32_t;
 typedef int64_t ogg_int64_t;
+typedef uint64_t ogg_uint64_t;
 
 #endif
 #endif
